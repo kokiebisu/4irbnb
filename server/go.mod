935accee24cd6f71085de5d48795c6e16f11ac52
Ken <a01056715@gmail.com>
2020-06-26 02:07:19 -0700

    built basic setup of server

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module github.com/kokiebiu/gonebnb/server
 
 go 1.14
+
+require (
+	github.com/gofiber/fiber v1.12.0 // indirect
+	github.com/lib/pq v1.7.0
+)
--- server/main.go
+++ server/main.go
@@ -4,11 +4,39 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber"
-	"github.com/kokiebisu/gonebnb/server/database"
+	"github.com/kokiebiu/gonebnb/server/database"
 )
 
-func setupRoutes(app *fiber.App) {
+// Stay represents a stay
+type Stay struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
 
+// Stays represent an array of stays
+type Stays struct {
+	Stays []Stay `json:"stays"`
+}
+
+// GetAllStays retrieves all the stays
+func GetAllStays(c *fiber.Ctx) {
+	c.Send("Get All Stays")
+}
+
+// GetStay retrieves a stay by the given id
+func GetStay(c *fiber.Ctx) {
+	c.Send("Get Stay")
+}
+
+// AddStay adds a new stay
+func AddStay(c *fiber.Ctx) {
+	c.Send("Add Stay")
+}
+
+// RemoveStay removes a stay by the given id
+func RemoveStay(c *fiber.Ctx) {
+	c.Send("Remove Stay")
 }
 
 func main() {
@@ -18,5 +46,12 @@ func main() {
 
 	app := fiber.New()
 
-	setupRoutes(app)
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+	v1.Get("/stay", GetAllStays)
+	v1.Get("/stay/:id", GetStay)
+	v1.Post("/stay", AddStay)
+	v1.Delete("/stay/:id", RemoveStay)
+
+	app.Listen(4000)
 }
