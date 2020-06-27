b7a7e64a74cc0338bfcf4e11fef26311a0dfc248
Ken <a01056715@gmail.com>
2020-06-26 20:26:15 -0700

    add user controller functions

--- api/models/user.go
+++ api/models/user.go
@@ -3,8 +3,11 @@ package models
 import (
 	"errors"
 	"time"
+	"html"
+	"strings"
 
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // User represents the structure of model User
@@ -16,29 +19,46 @@ type User struct {
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
 
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyPassword(hashedpassword string, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedpassword), []byte(password))
+}
+
+func (user *User) SavePassword() error {
+	hashedPassword, err := Hash(user.Password)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	return nil
+} 
+
 func (user *User) Prepare() {
 	user.ID = 0
-	user.Email = html.EscapeString(strings.TrimSpace(u.Email))
+	user.Email = html.EscapeString(strings.TrimSpace(user.Email))
 	user.CreatedAt = time.Now()
 	user.UpdatedAt = time.Now()
 }
 
 func (user *User) SaveUser(db *gorm.DB) (*User, error) {
-	err := db.Debug().Create(&u).Error
+	err := db.Debug().Create(&user).Error
 	if err != nil {
 		// Has to return something for first argument so an empty User object
 		return &User{}, err
 	}
-	return u, nil
+	return user, nil
 }
 
-func (user *User) FindAllUsers(db *gorm.DB) (*User, error) {
+func (user *User) FindAllUsers(db *gorm.DB) (*[]User, error) {
 	users := []User{}
 	// Check the User model
 	// If you find it then insert into &users
 	err := db.Debug().Model(&User{}).Find(&users).Error
 	if err != nil {
-		return &[]Users{}, err
+		return &[]User{}, err
 	}
 	return &users, err
 }
@@ -46,7 +66,7 @@ func (user *User) FindAllUsers(db *gorm.DB) (*User, error) {
 func (user *User) FindUserById(db *gorm.DB, uid int) (*User, error) {
 	// Check the User model
 	// If you find it then insert into &u
-	err := db.Debug().Model(&User{}).Where("id = ?", uid).Take(&u).Error
+	err := db.Debug().Model(&User{}).Where("id = ?", uid).Take(&user).Error
 	if err != nil {
 		return &User{}, err
 	}
@@ -56,18 +76,18 @@ func (user *User) FindUserById(db *gorm.DB, uid int) (*User, error) {
 	return user, err
 }
 
-func (user *User) UpdateUser(db *gorm.DB, uid int) {
-	err := user.HashPassword()
+func (user *User) UpdateUser(db *gorm.DB, uid int) (*User, error) {
+	err := user.SavePassword()
 	if err != nil {
-		log.Fatal(err)
+		return &User{}, err
 	}
 	// You want to assign the the result to an empty User object
 	db = db.Debug().Model(&User{}).Where("id = ?", uid).Take(&User{}).UpdateColumns(
 		map[string]interface{}{
-			"password": u.Password,
-			"email": u.Email,
+			"password": user.Password,
+			"email": user.Email,
 			"update_at": time.Now(),
-		}
+		},
 	)
 	if db.Error != nil {
 		return &User{}, db.Error
