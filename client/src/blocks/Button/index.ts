82c9cb24b30a2f4f489d109fd4129c8f0697ec94
Ken <a01056715@gmail.com>
2020-07-01 02:10:38 -0700

    configured circle ci

--- client/src/blocks/Button/index.ts
+++ client/src/blocks/Button/index.ts
@@ -9,7 +9,7 @@ const Button: any = styled.button`
   background-color: #7d7d7d;
   border-radius: 2px;
   border: solid 1px transparent;
-  color: #000;
+  color: #fff;
   cursor: pointer;
   font-size: 1rem;
   padding: 0.5rem 1.5rem;
