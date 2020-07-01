26e46796230d4cc4d728fa76f4a4f106e58d1b7b
Ken <a01056715@gmail.com>
2020-07-01 00:17:56 -0700

    added blocks & elements directory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Card from './blocks/Card';
 
 function App() {
-  return <h1>hellsfdo</h1>;
+  return (
+    <Card>
+      <Card.Header>hello</Card.Header>
+    </Card>
+  );
 }
 
 export default App;
