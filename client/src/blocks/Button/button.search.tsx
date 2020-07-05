cef50fdaf902d2db83df1f27ac3362da3b291161
Ken <a01056715@gmail.com>
2020-07-05 03:13:04 -0700

    created searchbutton

--- client/src/blocks/Button/button.search.tsx
+++ client/src/blocks/Button/button.search.tsx
@@ -1,7 +1,25 @@
+import React from 'react';
 import { BaseButton } from './button';
 import styled from 'styled-components';
+import { Flex } from '../layout';
 
-export const SearchButton = styled(BaseButton)`
+import { Icon } from '../../assets/svg/icon';
+import { Text } from '../../elements/Text';
+
+type props = { size: number; onPress: () => void };
+
+export default (props: props) => {
+  return (
+    <Wrapper {...props}>
+      <Flex alignCenter>
+        <StyledIcon name='search' />
+        <StyledText>Search</StyledText>
+      </Flex>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled(BaseButton)`
   background: -webkit-linear-gradient(
     right,
     rgb(230, 30, 77) 0%,
@@ -10,4 +28,25 @@ export const SearchButton = styled(BaseButton)`
   );
   border: none;
   color: white;
+`;
+
+const StyledIcon = styled(Icon)`
+  position: relative;
+  top: 1.5px;
+  margin-right: 7px;
+  & svg {
+    width: 14px;
+    height: 14px;
+    path {
+      fill: white;
+    }
+  }
+`;
+
+const StyledText = styled(Text)`
+  font-size: 15px;
+  letter-spacing: 0.5px;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto,
+    Helvetica Neue, sans-serif !important;
+  font-weight: 400;
 `;
--- client/src/elements/Text.ts
+++ client/src/elements/Text.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-export const H = styled(motion.h3)`
-  color: red;
+export const Text = styled(motion.p)`
+  margin: 0;
 `;
