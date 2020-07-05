8c9b8e992f0f01495ac925dcecb32b60b031f7a3
Ken <a01056715@gmail.com>
2020-07-05 00:03:18 -0700

    added airbnb fonts files

--- client/src/styles/fonts.ts
+++ client/src/styles/fonts.ts
@@ -1,5 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const Fonts = createGlobalStyle`
+import AirbnbCerealBlack from '../assets/fonts/AirbnbCerealBlack.woff';
+import AirbnbCerealBold from '../assets/fonts/AirbnbCerealBold.woff';
 
+export const Fonts = createGlobalStyle`
+    @font-face {
+        font-family: 'Airbnb-Cereal';
+        src: url(${AirbnbCerealBlack});
+    }
 `;
