b151a97cba9af64fbf23e2ee8631b9e9b7887763
Ken <a01056715@gmail.com>
2020-07-04 23:49:28 -0700

    converted primary button to search button

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -2,5 +2,13 @@ import React from 'react';
 import { addDecorator } from '@storybook/react';
 
 addDecorator((storyFn) => (
-  <div style={{ textAlign: 'center' }}>{storyFn()}</div>
+  <div
+    style={{
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+    {storyFn()}
+  </div>
 ));
