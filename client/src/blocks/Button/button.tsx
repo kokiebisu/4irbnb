f526181669b86ff8464329d7c2c298fc2c5883e7
Ken <a01056715@gmail.com>
2020-07-05 04:22:55 -0700

    refactored text element

--- client/src/blocks/Button/button.tsx
+++ client/src/blocks/Button/button.tsx
@@ -13,31 +13,31 @@ interface ButtonProps {
   size: number;
 }
 
+const Button = styled.button<ButtonProps>`
+  border-radius: 8px;
+  font-size: 18px;
+  border: none;
+  ${({ styles }) => styles}
+  ${({ size }) =>
+    (size === 3 &&
+      css`
+        padding: 14px 22px;
+      `) ||
+    (size === 5 &&
+      css`
+        padding: 15px;
+      `) ||
+    (size === 7 &&
+      css`
+        padding: 20px;
+      `)}
+`;
+
 export const BaseButton: React.FC<Props> = ({
   onPress,
   children,
   ...props
 }) => {
-  const Button = styled.button<ButtonProps>`
-    border-radius: 8px;
-    font-size: 18px;
-    border: none;
-    ${props.styles}
-    ${({ size }) =>
-      (size === 3 &&
-        css`
-          padding: 14px 22px;
-        `) ||
-      (size === 5 &&
-        css`
-          padding: 15px;
-        `) ||
-      (size === 7 &&
-        css`
-          padding: 20px;
-        `)}
-  `;
-
   return (
     <Button {...props} onClick={onPress}>
       {children}
