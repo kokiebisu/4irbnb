ce34380c2416e89052729f3c0c9830e9cdf886c8
Ken <a01056715@gmail.com>
2020-07-05 03:41:55 -0700

    refactored button component

--- client/src/blocks/Button/button.tsx
+++ client/src/blocks/Button/button.tsx
@@ -4,41 +4,42 @@ import styled, { css } from 'styled-components';
 interface Props {
   onPress: () => void;
   className?: string;
+  styles: any;
   size: number;
 }
 
 interface ButtonProps {
+  styles: any;
   size: number;
 }
 
-const Button = styled.button<ButtonProps>`
-  border-radius: 8px;
-  font-size: 18px;
-  border: none;
-
-  ${({ size }) =>
-    (size === 3 &&
-      css`
-        padding: 14px 22px;
-      `) ||
-    (size === 5 &&
-      css`
-        padding: 15px;
-      `) ||
-    (size === 7 &&
-      css`
-        padding: 20px;
-      `)}
-`;
-
 export const BaseButton: React.FC<Props> = ({
-  className,
   onPress,
   children,
-  size,
+  ...props
 }) => {
+  const Button = styled.button<ButtonProps>`
+    border-radius: 8px;
+    font-size: 18px;
+    border: none;
+    ${props.styles}
+    ${({ size }) =>
+      (size === 3 &&
+        css`
+          padding: 14px 22px;
+        `) ||
+      (size === 5 &&
+        css`
+          padding: 15px;
+        `) ||
+      (size === 7 &&
+        css`
+          padding: 20px;
+        `)}
+  `;
+
   return (
-    <Button size={size} className={className} onClick={onPress}>
+    <Button {...props} onClick={onPress}>
       {children}
     </Button>
   );
