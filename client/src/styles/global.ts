01c6dd694cf9d1f9abafe3097d176102f7f27800
Ken <a01056715@gmail.com>
2020-07-05 00:35:40 -0700

    successfully added fonts to storybook

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -1,5 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 export const GlobalStyles = createGlobalStyle`
+    ${normalize}
+    
+    html {
+        box-sizing: border-box;
+    }
 
+    body {
+        font-family: 'Airbnb-Cereal';
+        font-weight: 500;
+        font-size: 16px;
+    }
 `;
