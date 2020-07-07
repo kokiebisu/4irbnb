b1071fe4666947de72ed56916e1748b111d78b15
Ken <a01056715@gmail.com>
2020-07-06 22:39:07 -0700

    updated snapshots

--- client/src/styles/fonts.ts
+++ client/src/styles/fonts.ts
@@ -1,11 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 
-// import AirbnbCerealBlack from '../../public/assets/fonts/AirbnbCerealBlack.woff';
-// import AirbnbCerealExtraBold from '../../public/assets/fonts/AirbnbCerealExtraBold.woff';
-// import AirbnbCerealBold from '../../public/assets/fonts/AirbnbCerealBold.woff';
-// import AirbnbCerealMedium from '../../public/assets/fonts/AirbnbCerealMedium.woff';
-// import AirbnbCerealBook from '../../public/assets/fonts/AirbnbCerealBook.woff';
-// import AirbnbCerealLight from '../../public/assets/fonts/AirbnbCerealLight.woff';
 import AirbnbCerealBold from '../assets/fonts/AirbnbCereal-Bold.woff2';
 import AirbnbCerealMedium from '../assets/fonts/AirbnbCereal-Medium.woff2';
 import AirbnbCerealBook from '../assets/fonts/AirbnbCereal-Book.woff2';
