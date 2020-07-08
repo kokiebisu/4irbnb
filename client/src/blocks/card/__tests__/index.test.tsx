e12d6bc443efc7fcbd593c396cc777e773022b63
Ken <a01056715@gmail.com>
2020-07-08 00:10:28 -0700

    created test for card

---
+++ client/src/blocks/card/__tests__/index.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import renderer from 'react-test-renderer';
+import 'jest-styled-components';
+
+describe('Base Card', () => {
+  it('renders correctly', () => {
+    const testingData = {
+      text: 'test',
+    };
+
+    const tree = renderer.create(<Card {...testingData} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
