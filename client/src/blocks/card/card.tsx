3da9c98bbe58ff7e7477f5ff1306b31bdec9780d
Ken <a01056715@gmail.com>
2020-07-09 19:30:29 -0700

    created outline for card component

--- client/src/blocks/card/card.tsx
+++ client/src/blocks/card/card.tsx
@@ -4,6 +4,7 @@ import { css } from 'styled-components';
 // Elements
 import { Box } from '../../elements/Box';
 import { Text } from '../../elements/Text';
+import { Img } from '../../elements/Img';
 
 type CardProps = {
   image: string;
@@ -13,21 +14,32 @@ type CardProps = {
 
 export default ({ image, title, description, ...props }: CardProps) => {
   return (
-    <Box styles={box} {...props}>
-      <Box>
-        <img src={image} alt='card-image' />
+    <Box styles={wrapperStyles} {...props}>
+      <Box styles={imgwrapperStyles}>
+        <Img styles={imgStyles} src={image} alt='card-image' />
       </Box>
       <Box>
-        {title && <Text styles={title}>{image}</Text>}
+        {title && <Text styles={titleStyles}>{title}</Text>}
         <Text>{description}</Text>
       </Box>
     </Box>
   );
 };
 
-const box = css`
+const wrapperStyles = css`
   background: red;
-  border-radius: 5px;
+  border-radius: 16px;
+  max-height: min-content;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 2px 8px;
 `;
 
-const title = css``;
+const imgwrapperStyles = css`
+  object-fit: cover;
+`;
+
+const imgStyles = css`
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+`;
+
+const titleStyles = css``;
--- client/src/blocks/card/stories/card.title.stories.tsx
+++ client/src/blocks/card/stories/card.title.stories.tsx
@@ -4,6 +4,8 @@ import Card from '../card';
 import { Text } from '../../../elements/Text';
 
 import cardimg from '../../../assets/img/card.jpg';
+import styled from 'styled-components';
+import { sizes } from '../../../styles/breakpoints';
 
 export default {
   component: Card,
@@ -18,4 +20,26 @@ const cardData = {
   description: "Unplug or reconnect on a trip that's just a short drive away",
 };
 
-export const base = () => <Card {...cardData} />;
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 20px;
+  display: flex;
+
+  @media ${sizes.sm} {
+    display: grid;
+    align-items: center;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 15px;
+  }
+`;
+
+export const base = () => (
+  <Wrapper>
+    <Card {...cardData} />
+
+    <Card {...cardData} />
+
+    <Card {...cardData} />
+  </Wrapper>
+);
