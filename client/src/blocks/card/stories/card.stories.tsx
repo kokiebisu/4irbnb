5ee205396232bc37e87b440d4ff80a9e244d969c
Ken <a01056715@gmail.com>
2020-07-09 20:52:22 -0700

    added single card without title to story

--- client/src/blocks/card/stories/card.stories.tsx
+++ client/src/blocks/card/stories/card.stories.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import { css } from 'styled-components';
+
+// block
 import Card from '../card';
 
-import { Text } from '../../../elements/Text';
+// element
+import { Box } from '../../../elements/Box';
 
-// imgs
+// img
 import card1 from '../../../assets/img/card1.jpg';
 import card2 from '../../../assets/img/card2.jpg';
 import card3 from '../../../assets/img/card3.jpg';
 
-import { css } from 'styled-components';
+// breakpoint
 import { sizes } from '../../../styles/breakpoints';
-import { Box } from '../../../elements/Box';
 
 export default {
   component: Card,
-  title: 'Blocks/Card/WithTitle',
+  title: 'Blocks/Card',
   excludeStories: /.*Data$/,
+};
+
+// data
+const cardDataWithoutTitle = {
+  image: card1,
+  description: 'Unique activities we can do together, led by a world of hosts',
 };
 
 const cardData1 = {
@@ -36,33 +45,55 @@ const cardData3 = {
   description: 'Comfortable private places, with room for friends or family',
 };
 
-export const Single = () => (
-  <Box>
-    <Card {...cardData1} />
-  </Box>
-);
+export const SingleWithoutTitle = () => {
+  const extend = {
+    box: css`
+      width: 100%;
+      max-width: 350px;
+    `,
+  };
+  return (
+    <Box styles={extend.box}>
+      <Card {...cardDataWithoutTitle} />
+    </Box>
+  );
+};
 
-export const Multiple = () => (
-  <Box styles={extend.multiple}>
-    <Card {...cardData1} />
-    <Card {...cardData2} />
-    <Card {...cardData3} />
-  </Box>
-);
+export const SingleWithTitle = () => {
+  const extend = {
+    box: css`
+      width: 100%;
+      max-width: 350px;
+    `,
+  };
+  return (
+    <Box styles={extend.box}>
+      <Card {...cardData1} />
+    </Box>
+  );
+};
 
-const extend = {
-  single: css``,
-  multiple: css`
-    width: 100%;
-    height: 100%;
-    padding: 0 20px;
-    display: flex;
+export const Multiple = () => {
+  const extend = {
+    box: css`
+      width: 100%;
+      height: 100%;
+      padding: 0 20px;
+      display: flex;
 
-    @media ${sizes.sm} {
-      display: grid;
-      align-items: center;
-      grid-template-columns: repeat(3, 1fr);
-      column-gap: 15px;
-    }
-  `,
+      @media ${sizes.sm} {
+        display: grid;
+        align-items: center;
+        grid-template-columns: repeat(3, 1fr);
+        column-gap: 15px;
+      }
+    `,
+  };
+  return (
+    <Box styles={extend.box}>
+      <Card {...cardData1} />
+      <Card {...cardData2} />
+      <Card {...cardData3} />
+    </Box>
+  );
 };
