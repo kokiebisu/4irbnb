98d9f72eec10d35bee518e87e1a9a56a79e19736
Ken <a01056715@gmail.com>
2020-07-09 18:41:06 -0700

    created basic structure of card component with the necessary props

--- client/src/blocks/card/__tests__/index.test.tsx
+++ client/src/blocks/card/__tests__/index.test.tsx
@@ -3,10 +3,25 @@ import Card from '../card';
 import renderer from 'react-test-renderer';
 import 'jest-styled-components';
 
+// img
+import img from '../../../assets/img/card.jpg';
+
 describe('Base Card', () => {
-  it('renders correctly', () => {
+  it('renders correctly with title', () => {
     const testingData = {
-      text: 'test',
+      image: img,
+      tite: 'test title',
+      description: 'test description',
+    };
+
+    const tree = renderer.create(<Card {...testingData} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly without title', () => {
+    const testingData = {
+      image: img,
+      description: 'test description',
     };
 
     const tree = renderer.create(<Card {...testingData} />).toJSON();
