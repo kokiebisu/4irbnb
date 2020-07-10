f955273750c391139d2a8e50c54235976e5674b8
Ken <a01056715@gmail.com>
2020-07-09 20:52:22 -0700

    added single card without title to story

--- client/src/blocks/card/__tests__/index.test.tsx
+++ client/src/blocks/card/__tests__/index.test.tsx
@@ -4,12 +4,12 @@ import renderer from 'react-test-renderer';
 import 'jest-styled-components';
 
 // img
-import img from '../../../assets/img/card.jpg';
+import card1 from '../../../assets/img/card1.jpg';
 
 describe('Base Card', () => {
   it('renders correctly with title', () => {
     const testingData = {
-      image: img,
+      image: card1,
       tite: 'test title',
       description: 'test description',
     };
@@ -20,7 +20,7 @@ describe('Base Card', () => {
 
   it('renders correctly without title', () => {
     const testingData = {
-      image: img,
+      image: card1,
       description: 'test description',
     };
 
