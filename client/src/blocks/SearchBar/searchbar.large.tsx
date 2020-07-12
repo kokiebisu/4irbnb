868cf80f6dfcb6c997a53519cef8cd49c75e2a73
Ken <a01056715@gmail.com>
2020-07-12 00:04:47 -0700

    finished styling for searchbar large

--- client/src/blocks/SearchBar/searchbar.large.tsx
+++ client/src/blocks/SearchBar/searchbar.large.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { css } from 'styled-components';
+import theme from 'styled-theming';
+
+// color
+import { colorpallete } from '../../styles/colorpallete';
+
+// layout
+import { Flex } from '../../layout/flex';
 
 // element
 import { Box } from '../../elements/Box';
@@ -14,17 +21,52 @@ type SearchBarProps = {
 export default ({ ...props }: SearchBarProps) => {
   return (
     <BaseButton styles={extend.wrapper} {...props}>
-      <Icon name='search' />
-      <Box styles={extend.text}>
-        <Text>Where are you going?</Text>
-      </Box>
+      <Flex alignCenter>
+        <Icon name='search' styles={extend.icon} />
+        <Box styles={extend.text}>
+          <Text>Where are you going?</Text>
+        </Box>
+      </Flex>
     </BaseButton>
   );
 };
 
+const primary = theme('mode', {
+  default: colorpallete.gray__5,
+});
+
+const secondary = theme('mode', {
+  default: colorpallete.gray__7,
+});
+
 const extend = {
   wrapper: css`
-    background-color: red;
+    background-color: transparent;
+    box-shadow: rgb(0, 0, 0, 0.15) 0px 5px 12px;
+    height: 50px;
+    width: 100%;
+    padding: 0 10px 0 18px;
+    border-radius: 24px;
   `,
-  text: css``,
+  icon: css`
+    position: relative;
+    top: 2px;
+    margin-right: 16px;
+    & svg {
+      width: 16px;
+      height: 16px;
+      stroke: ${secondary};
+      & path {
+        fill: ${secondary};
+        stroke-width: 2;
+      }
+    }
+  `,
+  text: css`
+    & p {
+      color: ${primary};
+      font-size: 16px;
+      font-weight: 500;
+    }
+  `,
 };
