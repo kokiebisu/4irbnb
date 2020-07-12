42572bc1350bc62f6d1c2f3cf93b4693030e2b2a
Ken <a01056715@gmail.com>
2020-07-11 22:04:36 -0700

    successfully rendered mobile and normal searchbar to storybook

--- client/src/blocks/SearchBar/searchbar.tsx
+++ client/src/blocks/SearchBar/searchbar.tsx
@@ -4,15 +4,16 @@ import React from 'react';
 import SearchBarLarge from './searchbar.large';
 import SearchBarSmall from './searchbar.small';
 
-type SearchButtonProps = {
+type SearchBarProps = {
   type: string;
+  onPress: () => void;
 };
 
 interface typeProps {
   [key: string]: JSX.Element;
 }
 
-export const SearchButton = ({ type, ...props }: SearchButtonProps) => {
+export const SearchBar = ({ type, ...props }: SearchBarProps) => {
   const types: typeProps = {
     mobile: <SearchBarLarge {...props} />,
     normal: <SearchBarSmall {...props} />,
