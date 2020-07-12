24399e02e14d819088fb3d81e6e4d0479b1dc2fd
Ken <a01056715@gmail.com>
2020-07-12 00:04:47 -0700

    finished styling for searchbar large

--- client/src/styles/colorpallete.ts
+++ client/src/styles/colorpallete.ts
@@ -2,7 +2,7 @@ export const colorpallete = {
   white: '#ffffff',
   bluegreen__5: '#008489',
   gray__8: '#242424',
-  gray__6: '#333333',
+  gray__7: '#333333',
   gray__5: '#717171',
   gray__1: '#DDDDDD',
   red__5: '#FF385C',
