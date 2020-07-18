57d79bccc8623ddd73e28c6137bf7319f70d5e58
Ken <a01056715@gmail.com>
2020-07-18 02:23:48 -0700

    added large and small to size

--- client/src/blocks/Card/card.tsx
+++ client/src/blocks/Card/card.tsx
@@ -13,18 +13,28 @@ import { colorpallete } from '../../styles/colorpallete';
 type CardProps = {
   image: string;
   title?: string;
-  description: string;
+  description?: string;
+  subtitle?: string;
+  size?: string;
 };
 
-export default ({ image, title, description, ...props }: CardProps) => {
+export default ({
+  image,
+  title,
+  subtitle,
+  description,
+  size,
+  ...props
+}: CardProps) => {
   return (
     <Box styles={extend.wrapper} {...props}>
       <Box styles={extend.img}>
         <Img src={image} alt='card-image' />
       </Box>
-      <Box styles={extend.text}>
+      <Box styles={size === 'lg' ? extend.text__lg : extend.text}>
         {title && <Text styles={extend.title}>{title}</Text>}
-        <Text styles={extend.description}>{description}</Text>
+        {subtitle && <Text styles={extend.subtitle}>{subtitle}</Text>}
+        {description && <Text styles={extend.description}>{description}</Text>}
       </Box>
     </Box>
   );
@@ -60,15 +70,28 @@ const extend = {
       border-top-right-radius: 16px;
     }
   `,
+  text__lg: css`
+    width: 50%;
+    text-align: left;
+    padding: 0 18px 12px 18px;
+  `,
   text: css`
     padding: 0 18px 12px 18px;
   `,
   title: css`
     font-weight: 700;
     color: ${primary};
-    padding: 4px 0;
+    padding: 4px 0 2px 0;
+  `,
+  subtitle: css`
+    padding: 14px 0;
+    color: ${primary};
+    font-weight: 500;
+    line-height: 1.3;
+    font-size: 15px;
   `,
   description: css`
+    padding-top: 2px;
     font-size: 13px;
     font-weight: 300;
     color: ${secondary};
