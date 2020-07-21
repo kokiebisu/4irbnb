355145b0690f54b8e0c478b85583c50cf7374f09
Ken <a01056715@gmail.com>
2020-07-21 12:06:44 -0700

    modified storybook main so that it accepts all storybook.tsx

--- client/src/organisms/Notification/stories/notification.cookie.stories.tsx
+++ client/src/organisms/Notification/stories/notification.cookie.stories.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { CookieNotification } from './notification.cookie.stories';
+import { Notification } from '../notification';
 
 export default {
   title: 'Design Systems|Organisms/Notification/Cookie',
   parameters: {
-    component: CookieNotification,
+    component: Notification,
     componentSubtitle: 'Displays the cookie notification for the user',
   },
   excludeStories: /.*Data$/,
 };
 
 export const Default = () => {
-  return <CookieNotification />;
+  return <Notification type='cookie' />;
 };
