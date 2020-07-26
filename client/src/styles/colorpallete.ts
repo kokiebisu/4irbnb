094d506fe3179312fde3d52789553ab3dd2adb13
Kenichi Okiebisu <a01056715@gmail.com>
2020-07-26 14:43:21 -0700

    fixed small searchbar

--- client/src/styles/colorpallete.ts
+++ client/src/styles/colorpallete.ts
@@ -6,6 +6,8 @@ export const colorpallete = {
   gray__7: '#333333',
   gray__5: '#717171',
   gray__1: '#DDDDDD',
-  red__5: '#FF385C',
+  pink__4: '#E61E4D',
+  pink__5: '#E31C5F',
+  pink__6: '#D70466',
   black: '#030101',
 };
