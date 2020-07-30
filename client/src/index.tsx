84309dd765dd19d43afd0f071019584479860bce
Kenichi Okiebisu <a01056715@gmail.com>
2020-07-30 02:28:28 -0700

    fixed import

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Fonts } from './styles/fonts';
-import { GlobalStyles } from './styles/global';
+import { Fonts, GlobalStyles } from 'styles';
 
 ReactDOM.render(
   <React.StrictMode>
