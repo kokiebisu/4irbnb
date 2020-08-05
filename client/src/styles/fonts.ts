1a7691e2d6ca4b1ab09fb35fbe1a0002d6eecad0
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-05 00:45:42 -0700

    fixed font weight and size for cookie

--- client/src/styles/fonts.ts
+++ client/src/styles/fonts.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import AirbnbCerealExtraBold from 'assets/fonts/AirbnbCereal-ExtraBold.woff';
+import AirbnbCerealLight from 'assets/fonts/AirbnbCereal-Light.woff';
 import AirbnbCerealBold from 'assets/fonts/AirbnbCereal-Bold.woff2';
 import AirbnbCerealMedium from 'assets/fonts/AirbnbCereal-Medium.woff2';
 import AirbnbCerealBook from 'assets/fonts/AirbnbCereal-Book.woff2';
@@ -7,19 +9,36 @@ import AirbnbCerealBook from 'assets/fonts/AirbnbCereal-Book.woff2';
 export default createGlobalStyle`
     @font-face {
         font-family: 'Airbnb-Cereal';
+        font-weight: 900;
+        src: url(${AirbnbCerealExtraBold}) format("truetype");
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'Airbnb-Cereal';
         font-weight: 700;
-        src: url(${AirbnbCerealBold}) format("woff2");
+        src: url(${AirbnbCerealBold}) format("truetype");
+        font-style: normal;
     }
 
     @font-face {
         font-family: 'Airbnb-Cereal';
         font-weight: 500;
-        src: url(${AirbnbCerealMedium}) format("woff2");
+        src: url(${AirbnbCerealMedium}) format("truetype");
+        font-style: normal;
     }
 
     @font-face {
         font-family: 'Airbnb-Cereal';
         font-weight: 300;
-        src: url(${AirbnbCerealBook}) format("woff2");
+        src: url(${AirbnbCerealBook}) format("truetype");
+        font-style: normal;
     }
+
+    @font-face {
+        font-family: 'Airbnb-Cereal';
+        src: url(${AirbnbCerealLight}) format("woff");
+        font-style: normal;
+        font-weight: 100;
+    }
+
 `;
