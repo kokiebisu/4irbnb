77b0d4ccf5ae82284cc09b1d55ce21a331c8b846
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-06 20:29:53 -0700

    fixed color

--- client/src/molecules/Button/button.search.tsx
+++ client/src/molecules/Button/button.search.tsx
@@ -1,47 +1,64 @@
 import React from 'react';
 import { css } from 'styled-components';
 
-// breakpoints
-import { sizes } from 'styles';
-
 // atoms
 import { Icon, Button, Text, Box } from 'atoms';
 
 // type
 import { CustomProps } from 'molecules/Button';
+import { darken, lighten } from 'polished';
+import theme from 'styled-theming';
+import { colors } from 'styles';
 
 export default ({ name, ...props }: CustomProps) => {
   return (
     <Button styles={extend.wrapper} {...props}>
       <Box styles={extend.content}>
         <Icon styles={extend.icon} name='Search' />
-        <Text styles={extend.text}>{name}</Text>
       </Box>
     </Button>
   );
 };
 
+const backgroundLight = theme('mode', {
+  light: lighten(0.2, colors.pink),
+});
+
+const background = theme('mode', {
+  light: colors.pink,
+});
+
+const backgroundDark = theme('mode', {
+  light: darken(0.2, colors.pink),
+});
+
 const extend = {
   wrapper: css`
-    background: -webkit-linear-gradient(
-      right,
-      rgb(230, 30, 77) 0%,
-      rgb(227, 28, 95) 50%,
-      rgb(215, 4, 102) 100%
-    );
+    background: #ff385c;
     border: none;
+    padding: 8px;
     color: white;
+    border-radius: 50%;
+    &:hover {
+      background: -webkit-linear-gradient(
+        right,
+        rgb(230, 30, 77) 0%,
+        rgb(227, 28, 95) 50%,
+        rgb(215, 4, 102) 100%
+      );
+      transition: 2s ease-in-out;
+      -webkit-transition: 2s ease-in-out;
+    }
   `,
   content: css`
-    display: block;
-    @media ${sizes.sm} {
-      display: flex;
-      align-items: center;
-    }
+    height: 32px;
+    width: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   `,
   icon: css`
     position: relative;
-    top: 1.5px;
 
     & svg {
       width: 14px;
@@ -51,19 +68,6 @@ const extend = {
         fill: white;
         stroke-width: 2;
       }
-    }
-  `,
-  text: css`
-    display: none;
-    font-size: 15px;
-    letter-spacing: 0.5px;
-    font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto,
-      Helvetica Neue, sans-serif !important;
-    font-weight: 400;
-    padding-left: 7px;
-
-    @media ${sizes.sm} {
-      display: block;
     }
   `,
 };
