9cb758cbfc8f77d98c3654c3a02c02926952a048
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-08 00:58:18 -0700

    created basic structure

--- client/src/molecules/SearchBar/searchbar.full.tsx
+++ client/src/molecules/SearchBar/searchbar.full.tsx
@@ -15,26 +15,22 @@ export default ({ ...props }: SearchBarProps) => {
   return (
     <Box styles={extend.wrapper}>
       <Box styles={extend.options}>
-        {/* <Button
-          styles={extend.section.wrapper}
-          onPress={() => console.log('pressed')}>
-          sadf
-        </Button>
-        <Button
-          styles={extend.section.wrapper}
-          onPress={() => console.log('pressed')}>
-          sadf
-        </Button>
-        <Button
-          styles={extend.section.wrapper}
-          onPress={() => console.log('pressed')}>
-          sadf
-        </Button>
-        <Button
-          styles={extend.section.wrapper}
-          onPress={() => console.log('pressed')}>
-          sdaf
-        </Button> */}
+        <Box styles={extend.section.side}>
+          <Button onPress={() => console.log('pressed')}>sadf</Button>
+        </Box>
+        <Box styles={extend.seperator} />
+        <Box styles={extend.section.middle}>
+          <Box styles={extend.section.button}>
+            <Button onPress={() => console.log('pressed')}>sadf</Button>
+          </Box>
+          <Box styles={extend.section.button}>
+            <Button onPress={() => console.log('pressed')}>sadf</Button>
+          </Box>
+        </Box>
+        <Box styles={extend.seperator} />
+        <Box styles={extend.section.side}>
+          <Button onPress={() => console.log('pressed')}>sdaf</Button>
+        </Box>
       </Box>
       <Box styles={extend.button}>
         <CustomButton type='search' onPress={() => console.log('pressed')} />
@@ -51,6 +47,11 @@ const secondary = theme('mode', {
   light: darken(0.7, colors.gray),
 });
 
+const common = css`
+  background-color: white;
+  border-radius: 50px;
+`;
+
 const extend = {
   wrapper: css`
     width: 100%;
@@ -58,16 +59,58 @@ const extend = {
     height: 65px;
     border-radius: 50px;
     display: flex;
+
     box-shadow: rgb(0, 0, 0, 0.15) 0px 5px 12px;
   `,
   section: {
-    wrapper: css`
-      height: 100%;
-      width: 150px;
-      border-radius: 50px;
+    side: css`
+      flex: 1.5 0 0%;
+      &:first-child {
+        border-top-left-radius: 50px;
+        border-bottom-left-radius: 50px;
+      }
+      &:last-child {
+      }
+
+      button {
+        height: 100%;
+        width: 100%;
+        padding: 20px;
+        border-radius: 50px;
+        ${common};
+        &:hover {
+          background-color: gray;
+        }
+      }
+    `,
+    middle: css`
+      flex: 2 0 0%;
+      width: 100%;
+      display: flex;
+    `,
+    button: css`
+      flex: 1 0 0%;
+      button {
+        height: 100%;
+        width: 100%;
+        padding: 20px;
+        ${common};
+
+        &:hover {
+          background-color: gray;
+        }
+      }
     `,
   },
+  seperator: css`
+    width: 1px;
+    height: 32px;
+  `,
   options: css`
+    border-top-left-radius: inherit;
+    border-bottom-left-radius: inherit;
+    background-color: white;
+    display: flex;
     flex: 1 1 0%;
     height: 100%;
   `,
