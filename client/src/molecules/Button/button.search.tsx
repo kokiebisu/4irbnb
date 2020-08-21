71420c9ef2c0ba3cf50d38a352737edff1678923
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-20 23:01:10 -0700

    revamped search button

--- client/src/molecules/Button/button.search.tsx
+++ client/src/molecules/Button/button.search.tsx
@@ -6,46 +6,38 @@ import { Icon, Button, Text, Box } from 'atoms';
 
 // type
 import { CustomProps } from 'molecules/Button';
-import { darken, lighten } from 'polished';
-import theme from 'styled-theming';
-import { colors } from 'styles';
 
 export default ({ name, ...props }: CustomProps) => {
   return (
     <Button styles={extend.wrapper} {...props}>
       <Box styles={extend.content}>
         <Icon styles={extend.icon} name='Search' />
+        <Text styles={extend.text}>Search</Text>
       </Box>
     </Button>
   );
 };
 
-const backgroundLight = theme('mode', {
-  light: lighten(0.2, colors.pink),
-});
-
 const extend = {
   wrapper: css`
-    background-color: #ff385c;
+    background-color: rgb(215, 4, 102);
     border: none;
-    padding: 8px;
+    padding: 15px 22px;
     color: white;
-    border-radius: 50%;
+    border-radius: 8px;
     transition: 0.8s;
     &:hover {
-      background-color: rgb(215, 4, 102);
+      background-color: #ff385c;
     }
   `,
   content: css`
-    height: 32px;
-    width: 32px;
     display: flex;
     align-items: center;
     justify-content: center;
   `,
   icon: css`
     position: relative;
-
+    margin-right: 8px;
     & svg {
       width: 14px;
       height: 14px;
@@ -55,5 +47,9 @@ const extend = {
         stroke-width: 2;
       }
     }
+  `,
+  text: css`
+    font-weight: 300;
+    font-size: 16px;
   `,
 };
