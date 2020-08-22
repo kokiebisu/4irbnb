5b5fdb4c82ae007e2dc706582d09fbb2bb01d69e
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-22 02:33:13 -0700

    updated globe

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -5,19 +5,6 @@ export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
 
-export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: 'light',
-    // toolbar: {
-    //   icon: 'circlehollow',
-    //   // array of plain string values or MenuItem shape (see below)
-    //   items: ['light', 'dark'],
-    // },
-  },
-};
-
 const globalProvider = (Story, context) => {
   return (
     <div>
--- client/src/atoms/stories/button.stories.tsx
+++ client/src/atoms/stories/button.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
-// atoms
 import { Text, Button } from 'atoms';
 import { ElementProps } from 'atoms/Button';
 
