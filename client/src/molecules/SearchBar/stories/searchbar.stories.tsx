9ef419452868a929961fd6cfc9bb95bf6a24805d
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-22 16:09:59 -0700

    updated stories for searchbar components

--- client/src/molecules/SearchBar/stories/searchbar.stories.tsx
+++ client/src/molecules/SearchBar/stories/searchbar.stories.tsx
@@ -1,55 +1,46 @@
 import React from 'react';
+import { Story } from '@storybook/react/types-6-0';
 import { css } from 'styled-components';
-
-// element
 import { Box } from 'atoms';
-
-// block
 import { SearchBar } from 'molecules/SearchBar';
-
 import { sizes } from 'styles';
 
 export default {
-  title: 'Design Systems|Molecules/SearchBar',
-  parameters: {
-    component: SearchBar,
-    componentSubtitle: 'Displays the searchbars',
-  },
-  excludeStories: /.*Data$/,
+  title: 'Design Systems/SearchBar',
+  component: SearchBar,
+  argTypes: { onPress: { action: 'clicked' } },
 };
 
-export const Mobile = () => {
-  const extend = {
-    wrapper: css`
+interface SearchBarTemplateProps {
+  type: string;
+  onPress: () => void;
+  styles?: any;
+}
+
+const SearchBarTemplate: Story<SearchBarTemplateProps> = ({
+  styles,
+  ...args
+}) => (
+  <Box styles={styles}>
+    <SearchBar {...args} />
+  </Box>
+);
+
+export const mobile = SearchBarTemplate.bind({});
+mobile.args = {
+  type: 'mobile',
+  styles: css`
+    width: 100%;
+  `,
+};
+
+export const normal = SearchBarTemplate.bind({});
+normal.args = {
+  type: 'normal',
+  styles: css`
+    @media ${sizes.lg} {
       width: 100%;
-      margin: 0 15px;
-    `,
-  };
-  return (
-    <Box styles={extend.wrapper}>
-      <SearchBar type='mobile' onPress={() => console.log('hello!')} />
-    </Box>
-  );
+      max-width: 1560px;
+    }
+  `,
 };
-
-export const Normal = () => {
-  return <SearchBar type='normal' onPress={() => console.log('hello!')} />;
-};
-
-// export const Full = () => {
-//   const extend = {
-//     wrapper: css`
-//       margin: 0 80px;
-
-//       @media ${sizes.lg} {
-//         width: 100%;
-//         max-width: 1560px;
-//       }
-//     `,
-//   };
-//   return (
-//     <Box styles={extend.wrapper}>
-//       <SearchBar type='full' onPress={() => console.log('hello!')} />
-//     </Box>
-//   );
-// };
