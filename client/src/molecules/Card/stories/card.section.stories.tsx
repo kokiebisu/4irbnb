6b7a8496e4ff7c4c8232648d4a220b4b9d9c7039
Kenichi Okiebisu <a01056715@gmail.com>
2020-08-22 15:54:17 -0700

    updated section card stories

--- client/src/molecules/Card/stories/card.section.stories.tsx
+++ client/src/molecules/Card/stories/card.section.stories.tsx
@@ -1,38 +1,27 @@
 import React from 'react';
-import { css } from 'styled-components';
-
-// atoms
-import { Box } from 'atoms';
-
-// molecules
 import Card from 'molecules/Card/card.section';
+import { Story } from '@storybook/react/types-6-0';
 
 export default {
-  title: 'Design Systems|Molecules/Card/Section',
-  parameters: {
-    component: Card,
-    componentSubtitle: 'Displays a card that stretches full width',
-  },
-  excludeStories: /.*Data$/,
+  title: 'Design Systems/Card/Section',
+  component: Card,
 };
 
-const cardData = {
+interface CardTemplateProps {
+  styles?: any;
+  title?: string;
+  description?: string;
+  option?: string;
+}
+
+const SectionCardTemplate: Story<CardTemplateProps> = (args) => (
+  <Card {...args} />
+);
+
+export const blackLivesMatter = SectionCardTemplate.bind({});
+blackLivesMatter.args = {
   title: 'We stand with #BlackLivesMatter',
   description:
     'We believe in a world where everyone belongs. We reject all racism that stands in the way.',
   option: 'Donate',
-};
-
-export const BlackLivesMatter = () => {
-  const extend = {
-    wrapper: css`
-      width: 100%;
-      padding: 0 20px;
-    `,
-  };
-  return (
-    <Box styles={extend.wrapper}>
-      <Card {...cardData} />
-    </Box>
-  );
 };
