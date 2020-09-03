5a37efeb0d4c16efb17530baa1b49594138db80f
Kenichi Okiebisu <a01056715@gmail.com>
2020-09-03 16:09:58 -0700

    created atoms/List

--- client/src/molecules/SearchBar/__tests__/searchbar.large.test.tsx
+++ client/src/molecules/SearchBar/__tests__/searchbar.large.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { SearchBar } from 'molecules/SearchBar';
-import { large } from 'molecules/SearchBar/stories/searchbar.stories';
 
 describe('Large Searchbar', () => {
   it('renders text correctly', () => {
