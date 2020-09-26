2b210f917f927401f0c1ec501807918a74fe8bdd
Kenichi Okiebisu <a01056715@gmail.com>
2020-09-26 12:31:29 -0700

    made the fonts to work

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,10 +1,8 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/components/**/stories/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+};
--- ikd/src/atoms/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from 'atoms/Box';
-export * from 'atoms/Button';
-export * from 'atoms/Icon';
-export * from 'atoms/Img';
-export * from 'atoms/Text';
-export * from 'atoms/List';
