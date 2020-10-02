b014f24554cd2010029d6d3b64f9970346f088f3
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-01 21:46:26 -0700

    configured scss and fonts for storybook

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,8 +1,25 @@
+const path = require('path');
+
 module.exports = {
-  stories: [
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/components/**/stories/*.stories.@(js|jsx|ts|tsx)',
+  stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          query: {
+            name: '[name].[ext]',
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
