cfb6d20c0d842fb544ccd0a5db81aed4eeec7d04
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-02 12:04:26 -0700

    added extendTo property to button

--- client/components/atoms/button/button.component.tsx
+++ client/components/atoms/button/button.component.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import Link from 'next/link';
+import module from './button.module.scss';
 
-export const Button = ({ onPress, children, to, ...props }) => {
+interface ButtonProps {
+  onPress: () => void;
+  children: React.ReactNode;
+  to?: string;
+  extendTo?: string;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  onPress,
+  children,
+  to,
+  extendTo = '',
+}) => {
   if (to) {
     return (
-      <Link href='/' passHref>
+      <Link href={to} passHref>
         {children}
       </Link>
     );
   } else {
-    return <button onClick={onPress}>{children}</button>;
+    return (
+      <button className={module[extendTo]} onClick={onPress}>
+        {children}
+      </button>
+    );
   }
 };
