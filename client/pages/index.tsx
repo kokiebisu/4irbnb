de8546c27e05f39b2af690f566527b0d2b48cdf3
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-03 14:40:19 -0700

    converted footer styling into utility based

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -31,6 +31,7 @@ import { Modal } from 'components/organisms/modal/modal.component';
 import color from '../styles/color.module.scss';
 import shape from '../styles/shape.module.scss';
 import modal from '../styles/modal.module.scss';
+import font from '../styles/font.module.scss';
 
 const CovidNotice = () => {
   return (
@@ -302,21 +303,59 @@ const DestinationTrips = () => {
 
 const Footer = () => {
   return (
-    <footer className={styles['footer']}>
+    <footer
+      className={[
+        color['b-t--white__2'],
+        space['p-v--22'],
+        color['bg--white__1'],
+      ].join(' ')}>
       <div className={layout['container']}>
-        <div className={styles['footer__sections']}>
+        <div
+          className={[
+            font['b-b--white__1'],
+            styles['flex__footer--section'],
+          ].join(' ')}>
           {sections.map((section, index) => {
             return (
-              <div key={index} className={styles['footer__section']}>
-                <div className={styles['footer__section--name']}>
-                  <h4>{section.name}</h4>
+              <div
+                key={index}
+                className={[
+                  color['bt--white__2'],
+                  space['nf-m-t--20'],
+                  styles['p-b__footer--section'],
+                  styles['w__footer--section'],
+                ].join(' ')}>
+                <div>
+                  <h4
+                    className={[font['size--12'], font['uppercase']].join(' ')}>
+                    {section.name}
+                  </h4>
                 </div>
-                <div className={styles['footer__section--items']}>
+                <div
+                  className={[
+                    layout['block'],
+                    space['m-v--15'],
+                    space['m-h--0'],
+                    styles['grid__footer--items'],
+                  ].join(' ')}>
                   {section.items.map((item, index) => {
                     return (
-                      <div className={styles['footer__item']} key={index}>
+                      <div
+                        className={[
+                          space['m-v--15'],
+                          space['m-h--0'],
+                          space['m-v--15'],
+                          styles['m__footer--item'],
+                        ].join(' ')}
+                        key={index}>
                         <Link href={item.url}>
-                          <a>{item.name}</a>
+                          <a
+                            className={[
+                              font['size--14'],
+                              font['weight--300'],
+                            ].join(' ')}>
+                            {item.name}
+                          </a>
                         </Link>
                       </div>
                     );
@@ -326,37 +365,77 @@ const Footer = () => {
             );
           })}
         </div>
-        <div className={styles['footer__others']}>
-          <div className={styles['footer__buttons']}>
+        <div className={styles['flex__footer--others']}>
+          <div
+            className={[
+              layout['items-center'],
+              space['m-h--0'],
+              space['m-v--20'],
+            ].join(' ')}>
             <div className={styles['footer__button']}>
-              <button>
-                <Globe width={16} />
-                <p>English(CA)</p>
+              <button
+                className={[
+                  layout['items-center'],
+                  space['m-r--10'],
+                  color['bg--transparent'],
+                  color['b--0'],
+                ].join(' ')}>
+                <Globe width={16} className={space['m-r--5']} />
+                <u className={[font['size--14'], space['m-r--7']].join(' ')}>
+                  English(CA)
+                </u>
               </button>
             </div>
             <div className={styles['footer__button']}>
-              <button>
-                <p>$</p>
-                <p>CAD</p>
+              <button
+                className={[
+                  layout['items-center'],
+                  color['bg--transparent'],
+                  color['b--0'],
+                ].join(' ')}>
+                <u className={[font['size--14'], space['m-r--7']].join(' ')}>
+                  $
+                </u>
+                <u className={[font['size--14'], space['m-r--7']].join(' ')}>
+                  CAD
+                </u>
               </button>
             </div>
           </div>
-          <div className={styles['footer__left']}>
+          <div className={layout['items-center']}>
             <div className={styles['footer__rights']}>
-              <p>&copy; 2020 Airbnb, Inc. All rights reserved</p>
+              <p
+                className={[
+                  font['size--14'],
+                  font['weight--300'],
+                  layout['inline-block'],
+                ].join(' ')}>
+                &copy; 2020 Airbnb, Inc. All rights reserved
+              </p>
             </div>
-            <div className={styles['footer__services']}>
-              <div className={styles['footer__dot']}>&nbsp;&nbsp;· &nbsp;</div>
-              <div className={styles['footer__service']}>
-                <a>Privacy</a>
+            <div className={layout['items-center']}>
+              <div className={styles['block__footer--dot']}>
+                &nbsp;&nbsp;· &nbsp;
+              </div>
+              <div className={space['m-r--10']}>
+                <a
+                  className={[font['size--14'], font['weight--300']].join(' ')}>
+                  Privacy
+                </a>
               </div>
               <div>&nbsp;· &nbsp;</div>
-              <div className={styles['footer__service']}>
-                <a>&nbsp;Terms</a>
+              <div className={space['m-r--10']}>
+                <a
+                  className={[font['size--14'], font['weight--300']].join(' ')}>
+                  &nbsp;Terms
+                </a>
               </div>
               <div>&nbsp;· &nbsp;</div>
-              <div className={styles['footer__service']}>
-                <a>&nbsp;Sitemap</a>
+              <div className={space['m-r--10']}>
+                <a
+                  className={[font['size--14'], font['weight--300']].join(' ')}>
+                  &nbsp;Sitemap
+                </a>
               </div>
             </div>
           </div>
@@ -385,7 +464,7 @@ const LandingPage = () => {
       </div>
       <div
         className={[
-          color['bg--white'],
+          color['bg--white__0'],
           layout['fb--0'],
           layout['z--9999'],
           layout['block'],
