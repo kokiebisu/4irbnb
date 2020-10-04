5888917a3b5a70c64425fe51b5cadb1340ae3a65
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-03 18:51:23 -0700

    updated unit tests for cards and buttons

--- client/components/atoms/card/card.component.tsx
+++ client/components/atoms/card/card.component.tsx
@@ -17,19 +17,16 @@ interface mapProps {
   [key: string]: JSX.Element;
 }
 
-export const Card: React.FC<CardProps> = ({
-  to,
-  extendsTo,
-  type,
-  ...props
-}) => {
+export const Card: React.FC<CardProps> = ({ extendsTo, type, ...props }) => {
   const { card } = props;
   const types: mapProps = {
     landing: <LandingCard {...props} />,
   };
   return (
     <Link href={card.to}>
-      <a className={extendsTo}>{types[type]}</a>
+      <a data-testid='card' className={extendsTo}>
+        {types[type]}
+      </a>
     </Link>
   );
 };
