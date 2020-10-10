005c8d39d121a817d3efb82a4f65d92daad8b40b
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-10 15:37:15 -0700

    fixed stay section

--- client/components/atoms/card/card.horizontal.tsx
+++ client/components/atoms/card/card.horizontal.tsx
@@ -1,70 +1,82 @@
 import React from 'react';
 import layout from '../../../styles/layout.module.scss';
-import font from '../../../styles/font.module.scss';
-import color from '../../../styles/color.module.scss';
-import space from '../../../styles/space.module.scss';
 import shape from '../../../styles/shape.module.scss';
-import styles from './card.module.scss';
+import space from '../../../styles/space.module.scss';
+import color from '../../../styles/color.module.scss';
+import font from '../../../styles/font.module.scss';
 import { Heart } from '../../../public/svg/original';
 
-interface Props {
-  card: any;
-  save?: boolean;
-}
-
-export const HorizontalCard: React.FC<Props> = ({ card, save }) => {
+export const HorizontalCard = ({ card, save }) => {
   return (
-    <div className={[styles['w__horizontal']].join(' ')}>
-      <div className={[layout['relative']].join(' ')}>
-        <img className={[shape['br--12']].join(' ')} src={card.imgUrl} />
+    <div>
+      <div style={{ position: 'relative', paddingTop: '66.6%' }}>
         <div
-          className={[
-            space['p-v--8'],
-            space['p-h--10'],
-            layout['at--0'],
-            layout['justify-between'],
-            shape['w--full'],
-          ].join(' ')}>
-          <div
-            className={
-              card.superhost
-                ? [].join(' ')
-                : [color['c--white__0'], shape['hidden']].join(' ')
-            }>
-            <div
-              className={[
-                space['p-h--8'],
-                space['p-v--4'],
-                color['bg--white__1'],
-                shape['shadow--lg'],
-                shape['br--3'],
-              ].join(' ')}>
-              <p
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+          }}>
+          <div>
+            <div className={[layout['relative']].join(' ')}>
+              <div className={[shape['br--12']].join(' ')}>
+                <img
+                  className={[shape['br--12']].join(' ')}
+                  src={card.imgUrl}
+                />
+              </div>
+              <div
                 className={[
-                  font['size--12'],
-                  font['uppercase'],
-                  font['ls--3'],
+                  space['p-v--8'],
+                  space['p-h--10'],
+                  layout['at--0'],
+                  layout['justify-between'],
+                  shape['w--full'],
                 ].join(' ')}>
-                Superhost
-              </p>
+                <div
+                  className={
+                    card.superhost
+                      ? [].join(' ')
+                      : [color['c--white__0'], shape['hidden']].join(' ')
+                  }>
+                  <div
+                    className={[
+                      space['p-h--8'],
+                      space['p-v--4'],
+                      color['bg--white__1'],
+                      shape['shadow--lg'],
+                      shape['br--3'],
+                    ].join(' ')}>
+                    <p
+                      className={[
+                        font['size--12'],
+                        font['uppercase'],
+                        font['ls--3'],
+                      ].join(' ')}>
+                      Superhost
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className={
+                    save
+                      ? [color['c--white__0']].join(' ')
+                      : [color['c--white__0'], shape['hidden']].join(' ')
+                  }>
+                  <Heart
+                    fill='rgba(0, 0, 0, 0.5)'
+                    width={24}
+                    stroke='rgb(255, 255, 255)'
+                    strokeWidth={2}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <div
-            className={
-              save
-                ? [color['c--white__0']].join(' ')
-                : [color['c--white__0'], shape['hidden']].join(' ')
-            }>
-            <Heart
-              fill='rgba(0, 0, 0, 0.5)'
-              width={24}
-              stroke='rgb(255, 255, 255)'
-              strokeWidth={2}
-            />
           </div>
         </div>
       </div>
-      <div className={layout['items-center']}>
+      <div style={{ display: 'flex', marginTop: 6 }}>
         <div className={[space['m-r--3']].join(' ')}>
           <svg fill='#F5385D' width={14} viewBox='0 0 1000 1000'>
             <path d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z' />
@@ -97,6 +109,12 @@ export const HorizontalCard: React.FC<Props> = ({ card, save }) => {
       </div>
       <div>
         <p
+          style={{
+            maxHeight: 250,
+            whiteSpace: 'nowrap',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
           className={[
             font['ls--4'],
             font['weight--100'],
