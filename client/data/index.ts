5a9da8bd90e11d2f0dd568e1386f8c7fa78ed4b2
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-10 14:40:51 -0700

    created pagination carousel

---
+++ client/data/index.ts
@@ -0,0 +1,42 @@
+export const horizontals = [
+  {
+    imgUrl:
+      'https://a0.muscache.com/im/pictures/af97b312-1d07-4092-ac6d-a03fff6c5176.jpg?im_w=720',
+    superhost: true,
+    ratings: 4.93,
+    number_of_reviews: 238,
+    type: 'Farm stay',
+    to: '/',
+    title: 'The Moulton Meadow Farm: Seaview Ca...',
+  },
+  {
+    imgUrl:
+      'https://a0.muscache.com/im/pictures/af97b312-1d07-4092-ac6d-a03fff6c5176.jpg?im_w=720',
+    superhost: true,
+    ratings: 4.93,
+    number_of_reviews: 238,
+    type: 'Farm stay',
+    to: '/',
+    title: 'The Moulton Meadow Farm: Seaview Ca...',
+  },
+  {
+    imgUrl:
+      'https://a0.muscache.com/im/pictures/af97b312-1d07-4092-ac6d-a03fff6c5176.jpg?im_w=720',
+    superhost: true,
+    ratings: 4.93,
+    number_of_reviews: 238,
+    type: 'Farm stay',
+    to: '/',
+    title: 'The Moulton Meadow Farm: Seaview Ca...',
+  },
+  {
+    imgUrl:
+      'https://a0.muscache.com/im/pictures/af97b312-1d07-4092-ac6d-a03fff6c5176.jpg?im_w=720',
+    superhost: true,
+    ratings: 4.93,
+    number_of_reviews: 238,
+    type: 'Farm stay',
+    to: '/',
+    title: 'The Moulton Meadow Farm: Seaview Ca...',
+  },
+];
