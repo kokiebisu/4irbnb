9f53b7baed7564c032c82635392fd542a99be1f6
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-24 23:13:19 -0700

    organized structure for server

--- server/main.go
+++ server/main.go
@@ -6,12 +6,12 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/kokiebisu/airbnb/models"
+	"github.com/kokiebisu/airbnb/services"
 	_ "github.com/lib/pq"
 )
 
 // Database instance
-var db *sql.DB
+var Db *sql.DB
 
 // Database settings
 const (
@@ -45,99 +45,16 @@ func main() {
 	app := fiber.New()
 
 	// Get all records from postgreSQL
-	app.Get("/stays", func(c *fiber.Ctx) error {
-		// Insert Employee into database
-		rows, err := db.Query("SELECT * from stays")
-		if err != nil {
-			return c.JSON(&fiber.Map{
-				"success": false,
-				"err": err,
-			})
-		}
-		defer rows.Close()
-		result := models.Stays{}
-
-		for rows.Next() {
-			stay := models.Stay{}
-			if err := rows.Scan(&stay.ID, &stay.Imgurl, &stay.Superhost, &stay.Ratings); err != nil {
-				return err // Exit if we get an error
-			}
-
-			// Append Employee to Employees
-			result.Stays = append(result.Stays, stay)
-		}
-		// Return Employees in JSON format
-		return c.JSON(result)
-	})
+	app.Get("/stays", services.GetStays)
 
 	// Add record into postgreSQL
-	app.Post("/stays", func(c *fiber.Ctx) error {
-		// New Employee struct
-		u := new(models.Stay)
-
-		// Parse body into struct
-		if err := c.BodyParser(u); err != nil {
-			return c.Status(400).SendString(err.Error())
-		}
-
-		// Insert Employee into database
-		res, err := db.Query("INSERT INTO stays (imgurl, superhost) VALUES ($1, $2)", u.Imgurl, u.Superhost)
-		if err != nil {
-			return err
-		}
-
-		// Print result
-		log.Println(res)
-
-		// Return Employee in JSON format
-		return c.JSON(u)
-	})
+	app.Post("/stays", services.CreateStay)
 
 	// Update record into postgreSQL
-	app.Put("/stays", func(c *fiber.Ctx) error {
-		// New Employee struct
-		u := new(models.Stay)
-
-		// Parse body into struct
-		if err := c.BodyParser(u); err != nil {
-			return c.Status(400).SendString(err.Error())
-		}
-
-		// Insert Employee into database
-		res, err := db.Query("UPDATE stays SET imgurl=$1 WHERE id=$5", u.Imgurl, u.ID)
-		if err != nil {
-			return err
-		}
-
-		// Print result
-		log.Println(res)
-
-		// Return Employee in JSON format
-		return c.Status(201).JSON(u)
-	})
+	app.Put("/stays", services.UpdateStay)
 
 	// Delete record from postgreSQL
-	app.Delete("/stays", func(c *fiber.Ctx) error {
-		// New Employee struct
-		u := new(models.Stay)
-
-		// Parse body into struct
-		if err := c.BodyParser(u); err != nil {
-			return c.Status(400).SendString(err.Error())
-		}
-
-		// Insert Employee into database
-		res, err := db.Query("DELETE FROM stays WHERE id = $1", u.ID)
-		if err != nil {
-			return err
-		}
-
-		// Print result
-		log.Println(res)
-
-		// Return Employee in JSON format
-		return c.JSON("Deleted")
-	})
+	app.Delete("/stays", services.DeleteStay)
 
 	log.Fatal(app.Listen(":3000"))
 }
