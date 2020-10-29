a8d16d89aabe4518251fda385d7ae1fbfc70eedf
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-29 15:57:29 -0700

    added seed.sql to server

---
+++ client/helper/string.ts
@@ -0,0 +1,11 @@
+declare global {
+  interface String {
+    capitalize(): string;
+  }
+}
+
+String.prototype.capitalize = function () {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+export {};
