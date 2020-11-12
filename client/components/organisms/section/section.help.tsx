5d5309189e133e7a2acca39a36e3f197054bb0c9
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-12 01:38:41 -0800

    added help section to homes page

--- client/components/organisms/section/section.help.tsx
+++ client/components/organisms/section/section.help.tsx
@@ -1,19 +1,23 @@
 import { Layout } from '../../../layout/layout.component';
 import React from 'react';
 import { Bullet } from '../../../components/atoms/bullet/bullet.component';
+import { HelpSectionProps } from './props';
+import layout from '../../../styles/layout.module.scss';
+import font from '../../../styles/font.module.scss';
+import space from '../../../styles/space.module.scss';
 
-export const HelpSection = () => {
+export const HelpSection: React.FC<HelpSectionProps> = () => {
   const helps = ['support', 'tools', 'insights', 'education'];
   return (
-    <div>
-      <div>
-        <h1>We're here to help</h1>
+    <div className={[layout['flex']].join(' ')}>
+      <div style={{ width: '50%' }} className={[].join(' ')}>
+        <h1 className={[font['size--40']].join(' ')}>We're here to help</h1>
       </div>
-      <div>
+      <div style={{ width: '50%' }} className={[].join(' ')}>
         <div>
           {helps.map((help, index) => {
             return (
-              <div key={index}>
+              <div key={index} className={[space['m-b--24']].join(' ')}>
                 <Bullet type='help' help={help} />
               </div>
             );
