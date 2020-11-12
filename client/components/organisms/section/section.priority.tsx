7b217abea36dfdfc949f5b047781201e6eea8239
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-11 23:37:24 -0800

    added priority section to homes page

--- client/components/organisms/section/section.priority.tsx
+++ client/components/organisms/section/section.priority.tsx
@@ -1,11 +1,30 @@
 import { Layout } from '../../../layout/layout.component';
 import React from 'react';
 import { PrioritySectionProps } from './props';
+import { Card } from '../../../components/atoms/card/card.component';
+import space from '../../../styles/space.module.scss';
 
 export const PrioritySection: React.FC<PrioritySectionProps> = () => {
+  const priorities = ['protection', 'guidance', 'requirements'];
   return (
-    <Layout type='section' sectionType='homes'>
-      <div>yo</div>
+    <Layout type='section' sectionType='homes' title='Safety is our priority'>
+      <div className={[space['m-t--16']].join(' ')}>
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(3, 1fr)',
+            columnGap: 12,
+          }}
+          className={[].join(' ')}>
+          {priorities.map((priority, index) => {
+            return (
+              <div key={index}>
+                <Card type='priority' priority={priority} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Layout>
   );
 };
