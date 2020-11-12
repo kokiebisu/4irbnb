63144892d5af97f42d0f510c7947f25980c20b0c
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-12 02:02:56 -0800

    fixed layout for landing page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,21 +1,21 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import index from 'styles/index.module.scss';
-import layout from 'styles/layout.module.scss';
-import space from 'styles/space.module.scss';
+import index from '../styles/index.module.scss';
+import layout from '../styles/layout.module.scss';
+import space from '../styles/space.module.scss';
 import { categories, anywhere, onlines } from '../content';
 
-import { Modal } from 'components/organisms/modal/modal.component';
+import { Modal } from '../components/organisms/modal/modal.component';
 import color from '../styles/color.module.scss';
 import shape from '../styles/shape.module.scss';
 import font from '../styles/font.module.scss';
 import { useToggleState } from '../context/toggle';
-import { Section } from 'components/organisms/section/section.component';
-import { Footer } from 'components/organisms/footer/footer.component';
-import { MenuBar } from 'components/organisms/menubar/menubar.component';
-import { Layout } from 'layout/layout.component';
+import { Section } from '../components/organisms/section/section.component';
+import { Footer } from '../components/organisms/footer/footer.component';
+import { MenuBar } from '../components/organisms/menubar/menubar.component';
+import { Layout } from '../layout/layout.component';
 import { nearby } from '../data/stays';
 import { AnimatePresence, motion } from 'framer-motion';
-import { Header } from 'components/organisms/header/header.component';
+import { Header } from '../components/organisms/header/header.component';
 
 const CovidNotice = () => {
   return (
@@ -74,26 +74,39 @@ const LandingPage = () => {
       <div ref={wrapperRef}>
         <CovidNotice />
         <Section type='banner' />
-        <Layout type='section' spread>
+        <Layout type='section' sectionType='landing' spread>
           <Section type='nearby' items={nearby} />
         </Layout>
-        <Layout type='section' title='Live anywhere' spread>
+        <Layout
+          type='section'
+          sectionType='landing'
+          title='Live anywhere'
+          spread>
           <Section type='category' items={anywhere} />
         </Layout>
         <div className={space['m-v--32']}></div>
         <Layout
           dark
           spread
+          sectionType='landing'
           type='section'
           title='Meet Online Experiences'
           subtitle='Interactive activities you can do together, led by expert hosts.'>
-          <Section type='online' dark />
+          <Section type='online' sectionType='landing' dark />
         </Layout>
-        <Layout spread type='section' title='Join millions of hosts on Airbnb'>
-          <Section type='category' items={categories} />
+        <Layout
+          sectionType='landing'
+          spread
+          type='section'
+          title='Join millions of hosts on Airbnb'>
+          <Section type='category' sectionType='landing' items={categories} />
         </Layout>
-        <Layout spread type='section' title='Destinations for future trips'>
-          <Section type='destinations' />
+        <Layout
+          sectionType='landing'
+          spread
+          type='section'
+          title='Destinations for future trips'>
+          <Section type='destinations' sectionType='landing' />
         </Layout>
         <Footer spread />
         <Modal
