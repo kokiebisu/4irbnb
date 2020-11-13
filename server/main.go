9818b8a0669e1b65c2a18894a8f73660ce7b15d5
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-13 12:42:22 -0800

    Revert "modified server from scratch into a microserver"
    This reverts commit 0a6f3bd4b20fcedd0aa79d2c7f25dc2e7968be13.

--- server/main.go
+++ server/main.go
@@ -2,56 +2,92 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"time"
 
-	"github.com/kokiebisu/airbnb/handlers"
+	"github.com/kokiebisu/airbnb/models"
 	_ "github.com/lib/pq"
 )
 
 var db *sql.DB
 
 const (
-	host     = "localhost"
+	host     = "db"
 	port     = 5432 // Default port
 	user     = "postgres"
 	password = "password"
 	dbname   = "airbnb"
 )
 
-
+func init() {
+	var err error
+	db, err = sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname))
+	if err != nil {
+		panic(err)
+	}
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("You are connected to the database")
+}
 
 func main() {
-	l := log.New(os.Stdout, "api", log.LstdFlags)
-	uh := handlers.NewUsers(l)
-	sh := handlers.NewStays(l)
+	http.HandleFunc("/", greetings)
+	http.HandleFunc("/stays", GetStays)
 
-	sm := http.NewServeMux()
-	sm.Handle("/users", uh)
-	sm.Handle("/stays", sh)
-	
-	server := &http.Server{
-		Addr: ":8080",
-		Handler: sm,
-		IdleTimeout: 120*time.Second,
-		ReadTimeout: 1 *time.Second,
-		WriteTimeout: 1 *time.Second,
+	log.Fatal(http.ListenAndServeTLS(":443", "airbnb.crt", "airbnb.key", nil))
+}
+
+func greetings(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello world")
+}
+
+func GetStays(w http.ResponseWriter, req *http.Request) {
+	var rows *sql.Rows
+	var err error
+	uniqueTypes := []string{"farm_stay", "earth_house", "barn", "tent"}
+	typeParams := req.URL.Query()["type"]
+	if len(typeParams) == 0 {
+		rows, err = db.Query("SELECT * FROM stay")
+	} else if typeParams[0] == "unique" {
+		rows, err = db.Query("SELECT * FROM stay WHERE type = $1 OR type = $2 OR type = $3 OR type = $4", uniqueTypes[0], uniqueTypes[1], uniqueTypes[2], uniqueTypes[3])
+	} else {
+		rows, err = db.Query("SELECT * FROM stay WHERE type = $1", typeParams[0])
 	}
 
-	go func() {
-		err := server.ListenAndServe()
-		if err != nil {
-			l.Fatal(err)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	if err != nil {
+		json.NewEncoder(w).Encode(struct {
+			success bool
+			err error
+		}{false, err})
+	}
+
+	defer rows.Close()
+	var result []models.Stay
+	for rows.Next() {
+		stayInfo := models.StayInfo{}
+	
+		if err := rows.Scan(&stayInfo.ID, &stayInfo.Imgurl, &stayInfo.Superhost, &stayInfo.Ratings, &stayInfo.NumberOfReviews, &stayInfo.Type, &stayInfo.Location, &stayInfo.Title); err != nil {
+			panic(err) // Exit if we get an error
 		}
-	}()
 
-	sigChan := make(chan os.Signal)
-	signal.Notify(sigChan, os.Interrupt)
-	signal.Notify(sigChan, os.Kill)
+		stay := models.Stay{
+			Card: stayInfo,
+			To: stayInfo.ID,
+		}
 
-	sig := <- sigChan
-	l.Println("Received terminate, graceful shutdown", sig)
+		// Append Employee to Employees
+		result = append(result, stay)
+
+	}
+	// Return Employees in JSON format
+	json.NewEncoder(w).Encode(result)
 }
