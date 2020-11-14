c1362aa120cf90018daa2daa66315f53009ba3d2
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-13 22:47:59 -0800

    fixed loading animation

--- client/components/animation/animation.loading.tsx
+++ client/components/animation/animation.loading.tsx
@@ -1,72 +1,25 @@
-import { motion } from 'framer-motion';
 import React from 'react';
 import { LoadingAnimationProps } from './props';
+import animation from './animation.module.scss';
 
 export const LoadingAnimation: React.FC<LoadingAnimationProps> = () => {
   return (
-    <motion.div
-      style={loadingContainer}
-      variants={loadingContainerVariants}
-      initial='start'
-      animate='end'>
-      <motion.span
-        style={loadingCircle}
-        variants={loadingCircleVariants}
-        transition={loadingCircleTransition}
+    <div className={[animation['circle__wrapper']].join(' ')}>
+      <span
+        className={[animation['circle__content'], animation['circle--1']].join(
+          ' '
+        )}
       />
-      <motion.span
-        style={loadingCircle}
-        variants={loadingCircleVariants}
-        transition={loadingCircleTransition}
+      <span
+        className={[animation['circle__content'], animation['circle--2']].join(
+          ' '
+        )}
       />
-      <motion.span
-        style={loadingCircle}
-        variants={loadingCircleVariants}
-        transition={loadingCircleTransition}
+      <span
+        className={[animation['circle__content'], animation['circle--3']].join(
+          ' '
+        )}
       />
-    </motion.div>
+    </div>
   );
-};
-
-const loadingContainer = {
-  width: '2rem',
-  height: '2rem',
-  display: 'flex',
-  justifyContent: 'space-around',
-};
-
-const loadingContainerVariants = {
-  start: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-  end: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const loadingCircle = {
-  display: 'block',
-  width: '0.5rem',
-  height: '0.5rem',
-  backgroundColor: 'white',
-  borderRadius: '0.25rem',
-};
-
-const loadingCircleVariants = {
-  start: {
-    y: '50%',
-  },
-  end: {
-    y: '150%',
-  },
-};
-
-const loadingCircleTransition = {
-  duration: 0.5,
-  yoyo: Infinity,
-  ease: 'easeInOut',
 };
