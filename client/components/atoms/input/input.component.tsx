9e5dd21d655956f5058e821125c16f73c270b910
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-14 01:03:41 -0800

    added documentation to input components

--- client/components/atoms/input/input.component.tsx
+++ client/components/atoms/input/input.component.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+
+/**
+ * Components
+ */
 import { TextInput } from './input.text';
+
+/**
+ * Props
+ */
 import { InputProps } from './props';
 
+/**
+ * Bundles the input components
+ * @param {string} type - Specifies the type of input component
+ */
 export const Input: React.FC<InputProps> = ({ type, ...props }) => {
   const types = {
     text: <TextInput {...props} />,
