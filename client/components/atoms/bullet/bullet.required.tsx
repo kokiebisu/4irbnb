9467ac9d59f40eae8e1ea990c78a70035faf7d78
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-14 23:22:22 -0800

    added exclamation vector and required bullet

---
+++ client/components/atoms/bullet/bullet.required.tsx
@@ -0,0 +1,23 @@
+import { Exclamation } from '../../../public/svg/original';
+import React from 'react';
+import { RequiredBulletProps } from './props';
+import font from '../../../styles/font.module.scss';
+import layout from '../../../styles/layout.module.scss';
+import space from '../../../styles/space.module.scss';
+
+export const RequiredBullet: React.FC<RequiredBulletProps> = ({ name }) => {
+  return (
+    <div className={[layout['flex'], layout['items-center']].join(' ')}>
+      <div className={[space['m-r--4']].join(' ')}>
+        <Exclamation width={16} stroke='#C54A32' strokeWidth={2} />
+      </div>
+      <div>
+        <p
+          style={{ color: '#C54A32' }}
+          className={[font['size--12']].join(' ')}>
+          {name} is required
+        </p>
+      </div>
+    </div>
+  );
+};
