047e93256bff733c8ef5661dbc2f124f2d109e26
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-16 23:46:49 -0800

    added login functionality to server

--- server/repository/user.repo.go
+++ server/repository/user.repo.go
@@ -10,6 +10,7 @@ import (
 type UserRepository interface {
 	Save(user *entity.User) (*entity.User, error)
 	FindAll() ([]*entity.User, error)
+	FindByEmail(s string) (*entity.User, error)
 }
 
 type repo struct {
@@ -23,7 +24,7 @@ func NewUserRepository() UserRepository {
 }
 
 func (r *repo) Save(user *entity.User) (*entity.User, error) {
-	query := `INSERT INTO "user" (email, firstname, lastname, password) VALUES ($1, $2, $3, $4) returning *`
+	query := `INSERT INTO "user" (email, firstname, lastname, password) VALUES ($1, $2, $3, $4)`
 	_, err := r.db.Exec(query, (*user).Email, (*user).FirstName, (*user).LastName, (*user).Password)
 	if err != nil {
 		return nil, err
@@ -49,4 +50,19 @@ func (r *repo) FindAll() ([]*entity.User, error) {
 	}
 
 	return users, nil
+}
+
+func (r *repo) FindByEmail(email string) (*entity.User, error) {
+	rows, err := r.db.Query(`SELECT * FROM "user" WHERE email = $1`, email)
+	if err != nil {
+		return nil, err
+	}
+	var user entity.User
+	for rows.Next() {
+		err := rows.Scan(&user.ID, &user.Email, &user.FirstName, &user.LastName, &user.Password)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &user, nil
 }
