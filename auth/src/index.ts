83018f1c4575c86921c6b132ee7e1c2367aaca83
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-23 13:17:52 -0800

    added basic error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { currentUserRouter } from './routes/current_user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
