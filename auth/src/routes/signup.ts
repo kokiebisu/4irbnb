6e83368b073d7d8c3f0159cf9844e7773dcd0ae4
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-23 13:40:13 -0800

    covered not found error

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,15 +1,20 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { DatabaseConnectionError } from '../errors/database';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  const { email, password } = req.body;
-
-  console.log('Creating a user...');
-  throw new DatabaseConnectionError();
-
-  res.send({});
-});
+router.post(
+  '/api/users/signup',
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body;
+      console.log('Creating a user...');
+      throw new Error();
+      res.send({});
+    } catch (err) {
+      next(new DatabaseConnectionError());
+    }
+  }
+);
 
 export { router as signupRouter };
