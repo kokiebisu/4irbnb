7ee0599571bd1a5fd86bce17e6d0a3a7cfb6cf50
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-23 13:40:13 -0800

    covered not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,10 @@
-import express, { json } from 'express';
+import express, { json, NextFunction } from 'express';
 import { currentUserRouter } from './routes/current_user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error';
+import { NotFoundError } from './errors/not_found';
 
 const app = express();
 
@@ -13,6 +14,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async (req, res, next: NextFunction) => {
+  next(new NotFoundError());
+});
 
 app.use(errorHandler);
 
