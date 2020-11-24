b6fe0776f046852a401b53bea214a21bd573eaec
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-23 16:11:55 -0800

    added password helper

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,20 +1,25 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { BadRequestError } from '../errors/bad_request';
 import { DatabaseConnectionError } from '../errors/database';
+import { User } from '../models/user';
 
 const router = express.Router();
 
-router.post(
-  '/api/users/signup',
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { email, password } = req.body;
-      console.log('Creating a user...');
-      throw new Error();
-      res.send({});
-    } catch (err) {
-      next(new DatabaseConnectionError());
-    }
+router.post('/api/users/signup', async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new BadRequestError('Email in use');
   }
-);
+
+  const user = User.build({ email, password });
+  await user.save();
+  // throw new DatabaseConnectionError();
+
+  // send jwt, token
+  res.status(201).send(user);
+});
 
 export { router as signupRouter };
