f168dcf272078dd788a2c64d8ab8c954f2fb1351
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-25 01:34:00 -0800

    converted login to use usefetch hooks

--- client/hooks/useFetch.ts
+++ client/hooks/useFetch.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-export const useFetch = ({ url, method, body, onSuccess }) => {
+export const useFetch = ({ url, method, body, onSuccess, onFail }) => {
   const doFetch = async () => {
     try {
       const response = await axios[method](url, body);
       return response.data;
     } catch (err) {
-      console.log('err log: ');
+      onFail();
     }
   };
   if (onSuccess) {
