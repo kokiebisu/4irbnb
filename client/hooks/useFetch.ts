90f1429fcf03fcfed6f18386643f581552f8d903
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-25 13:28:28 -0800

    added login feature

--- client/hooks/useFetch.ts
+++ client/hooks/useFetch.ts
@@ -5,24 +5,31 @@ export const useFetch = ({
   url,
   method,
   body,
+  triggerLoading,
   onSuccess,
   onFail,
 }: {
   url: string;
   method: string;
   body: any;
+  triggerLoading: (state: boolean) => void;
   onSuccess: (data?: any) => void;
   onFail?: () => void;
 }) => {
   const doFetch = async () => {
     try {
+      triggerLoading(true);
       const response = await axios[method](url, body);
+      console.log('response: ', response.data);
       if (onSuccess) {
         onSuccess(response.data);
       }
       return response.data;
     } catch (err) {
-      onFail();
+      setTimeout(() => {
+        triggerLoading(false);
+        onFail();
+      }, 2000);
     }
   };
   return doFetch;
