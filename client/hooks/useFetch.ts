3ee4c90f94a3f908174b83377337abc9f3ba0805
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-25 01:42:51 -0800

    fix

--- client/hooks/useFetch.ts
+++ client/hooks/useFetch.ts
@@ -1,6 +1,18 @@
 import axios from 'axios';
 
-export const useFetch = ({ url, method, body, onSuccess, onFail }) => {
+export const useFetch = ({
+  url,
+  method,
+  body,
+  onSuccess,
+  onFail,
+}: {
+  url: string;
+  method: string;
+  body: any;
+  onSuccess: () => void;
+  onFail?: () => void;
+}) => {
   const doFetch = async () => {
     try {
       const response = await axios[method](url, body);
