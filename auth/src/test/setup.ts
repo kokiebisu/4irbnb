f60d6acd1e900f1031333a374b40557df924819f
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-26 14:39:49 -0800

    added signup helper to test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,11 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import server from '../app';
 
 let mongo: any;
 
 // runs before all tests
 beforeAll(async () => {
+  process.env.JWT_KEY = 'test';
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
@@ -17,7 +17,6 @@ beforeAll(async () => {
 
 // runs before each test
 beforeEach(async () => {
-  process.env.JWT_KEY = 'test';
   const collections = await mongoose.connection.db.collections();
   for (let collection of collections) {
     await collection.deleteMany({});
