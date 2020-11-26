c603c82574d844e52338150e814c2442bd6049c2
Kenichi Okiebisu <a01056715@gmail.com>
2020-11-26 14:39:49 -0800

    added signup helper to test

--- auth/src/routes/__test__/current_user.test.ts
+++ auth/src/routes/__test__/current_user.test.ts
@@ -1,21 +1,15 @@
 import request from 'supertest';
 import server from '../../app';
+import { signup } from '../../test/helper';
 
 describe('Current user handler', () => {
   it('responds with proper email of the current user', async () => {
-    const signupResponse = await request(server)
-      .post('/api/users/signup')
-      .send({
-        email: 'test@gmail.com',
-        password: 'password',
-      });
-
-    const cookie = signupResponse.get('Set-Cookie');
+    const cookie = await signup();
 
     const response = await request(server)
       .get('/api/users/currentuser')
       .set('Cookie', cookie)
-      .send({});
+      .send();
 
     expect(response.body.currentUser.email).toEqual('test@gmail.com');
   });
