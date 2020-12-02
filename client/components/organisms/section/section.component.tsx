b81833d5a81441ac6fdb3e97969fa52c5b3d39fc
Kenichi Okiebisu <a01056715@gmail.com>
2020-12-02 13:14:30 -0800

    modified onlinehost layout

--- client/components/organisms/section/section.component.tsx
+++ client/components/organisms/section/section.component.tsx
@@ -1,34 +1,35 @@
-import React from 'react';
+import React from "react";
 
 /**
  * Components
  */
-import { CategorySection } from './section.category';
-import { PanelSection } from './section.panel';
-import { StaySection } from './section.stay';
-import { CharacteristicsSection } from './section.characteristics';
-import { DescriptionSection } from './section.description';
-import { SectionProps } from './props';
-import { ArrangementsSection } from './section.arrangements';
-import { AmenitySection } from './section.amenities';
-import { ReviewsSection } from './section.reviews';
-import { HostSection } from './section.host';
-import { KnowSection } from './section.know';
-import { OtherSection } from './section.other';
-import { NearbySection } from './section.nearby';
-import { DestinationsSection } from './section.destinations';
-import { OnlineSection } from './section.online';
-import { HomesSection } from './section.homes';
-import { AlsoSection } from './section.also';
-import { ParticipateSection } from './section.participate';
-import { BringSection } from './section.bring';
-import { ExperiencesSection } from './section.experiences';
-import { AvailableSection } from './section.available';
-import { AllSection } from './section.all';
-import { PrioritySection } from './section.priority';
-import { HelpSection } from './section.help';
-import { ReadySection } from './section.ready';
-import { WorksSection } from './section.works';
+import { CategorySection } from "./section.category";
+import { PanelSection } from "./section.panel";
+import { StaySection } from "./section.stay";
+import { CharacteristicsSection } from "./section.characteristics";
+import { DescriptionSection } from "./section.description";
+import { SectionProps } from "./props";
+import { ArrangementsSection } from "./section.arrangements";
+import { AmenitySection } from "./section.amenities";
+import { ReviewsSection } from "./section.reviews";
+import { HostSection } from "./section.host";
+import { KnowSection } from "./section.know";
+import { OtherSection } from "./section.other";
+import { NearbySection } from "./section.nearby";
+import { DestinationsSection } from "./section.destinations";
+import { OnlineSection } from "./section.online";
+import { HomesSection } from "./section.homes";
+import { AlsoSection } from "./section.also";
+import { ParticipateSection } from "./section.participate";
+import { BringSection } from "./section.bring";
+import { ExperiencesSection } from "./section.experiences";
+import { AvailableSection } from "./section.available";
+import { AllSection } from "./section.all";
+import { PrioritySection } from "./section.priority";
+import { HelpSection } from "./section.help";
+import { ReadySection } from "./section.ready";
+import { WorksSection } from "./section.works";
+import { SharingSection } from "./section.sharing";
 
 interface mapProps {
   [key: string]: JSX.Element;
@@ -70,6 +71,7 @@ export const Section: React.FC<SectionProps> = ({
     help: <HelpSection {...props} />,
     ready: <ReadySection {...props} />,
     works: <WorksSection {...props} />,
+    sharing: <SharingSection {...props} />,
   };
   return <div className={extendsTo}>{types[type]}</div>;
 };
--- client/pages/d/onlinehost.tsx
+++ client/pages/d/onlinehost.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import { Banner } from '../../components/organisms/banner/banner.component';
-import { Header } from '../../components/organisms/header/header.component';
-import { Section } from '../../components/organisms/section/section.component';
-import layout from '../../styles/layout.module.scss';
+import React from "react";
+import { Banner } from "../../components/organisms/banner/banner.component";
+import { Header } from "../../components/organisms/header/header.component";
+import { Section } from "../../components/organisms/section/section.component";
+import layout from "../../styles/layout.module.scss";
+import { Footer } from "../../components/organisms/footer/footer.component";
+import { Layout } from "../../layout/layout.component";
 
 const OnlineHost = () => {
   return (
     <div>
       <div>
-        <Header type='onlinehost' />
+        <Header type="onlinehost" />
       </div>
       <div>
-        <div className={[layout['container']].join(' ')}>
-          <Banner type='onlinehost' />
+        <div className={[layout["container"]].join(" ")}>
+          <Banner type="onlinehost" />
         </div>
       </div>
-      <div className={[layout['container']].join(' ')}>
-        <Section type='works' />
+      <div className={[layout["container"]].join(" ")}>
+        <Section type="works" />
+      </div>
+      <div>
+        <Section type="sharing" />
+      </div>
+      <div>
+        <Footer />
       </div>
     </div>
   );
