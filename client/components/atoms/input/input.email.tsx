a022048544a27044f3c9e6c9407ce638482798b8
Kenichi Okiebisu <a01056715@gmail.com>
2020-12-19 18:33:46 -0800

    refactor: converted input type to variant

--- client/components/atoms/input/input.email.tsx
+++ client/components/atoms/input/input.email.tsx
@@ -1,25 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-/**
- * Styles
- */
-import shape from '../../../styles/shape.module.scss';
-import color from '../../../styles/color.module.scss';
-import space from '../../../styles/space.module.scss';
-import layout from '../../../styles/layout.module.scss';
-import font from '../../../styles/font.module.scss';
-import input from './input.module.scss';
+/** styles */
+import shape from "../../../styles/shape.module.scss";
+import color from "../../../styles/color.module.scss";
+import space from "../../../styles/space.module.scss";
+import layout from "../../../styles/layout.module.scss";
+import font from "../../../styles/font.module.scss";
 
-/**
- * Props
- */
-import { EmailInputProps } from './props';
+/** styles */
+import input from "./input.module.scss";
+import { styleInput, styleLabel, styleContainer } from "./styling.text";
 
-/**
- * Styling
- */
-import { styleInput, styleLabel, styleContainer } from './styling.text';
-import { checkEmail } from '../../../helper/auth';
+/** helpers */
+import { checkEmail } from "../../../helper/auth";
 
 /**
  * Renders the text input component
@@ -30,12 +23,12 @@ import { checkEmail } from '../../../helper/auth';
  * @param {string} direction - direction in which the input if attached to another
  * @param {string} inputType - Whether if the input is text-based or select-based
  */
-export const EmailInput: React.FC<EmailInputProps> = ({
-  handleChange,
-  value,
-  direction,
-  errors = false,
-}) => {
+export const EmailInput: React.FC<{
+  handleChange?: any;
+  value?: string;
+  direction?: string;
+  errors?: boolean;
+}> = ({ handleChange, value, direction, errors = false }) => {
   const [fieldActive, setFieldActive] = useState(false);
 
   const activateField = () => {
@@ -48,22 +41,22 @@ export const EmailInput: React.FC<EmailInputProps> = ({
 
   const renderShape = () => {
     switch (direction) {
-      case 'top':
+      case "top":
         return [
-          color['b-b--white__3'],
-          color['b-l--white__3'],
-          color['b-r--white__3'],
-          shape['bbr--10'],
-        ].join(' ');
-      case 'bottom':
+          color["b-b--white__3"],
+          color["b-l--white__3"],
+          color["b-r--white__3"],
+          shape["bbr--10"],
+        ].join(" ");
+      case "bottom":
         return [
-          color['b-t--white__3'],
-          color['b-l--white__3'],
-          color['b-r--white__3'],
-          shape['btr--10'],
-        ].join(' ');
+          color["b-t--white__3"],
+          color["b-l--white__3"],
+          color["b-r--white__3"],
+          shape["btr--10"],
+        ].join(" ");
       default:
-        return [color['b--white__3'], shape['br--10']].join(' ');
+        return [color["b--white__3"], shape["br--10"]].join(" ");
     }
   };
 
@@ -71,57 +64,60 @@ export const EmailInput: React.FC<EmailInputProps> = ({
     <div
       style={{ height: 60 }}
       className={`${[
-        input['outside'],
-        layout['relative'],
-        space['p-v--6'],
-        space['p-h--12'],
-        layout['items-center'],
-      ].join(' ')} ${renderShape()} ${styleContainer(
+        input["outside"],
+        layout["relative"],
+        space["p-v--6"],
+        space["p-h--12"],
+        layout["items-center"],
+      ].join(" ")} ${renderShape()} ${styleContainer(
         errors,
         fieldActive,
         checkEmail(value)
-      )}`}>
+      )}`}
+    >
       <div
         style={{
-          position: 'relative',
-          height: '100%',
-          width: '100%',
-        }}>
+          position: "relative",
+          height: "100%",
+          width: "100%",
+        }}
+      >
         <input
           autoFocus={true}
-          id='email'
-          name='email'
-          type='text'
+          id="email"
+          name="email"
+          type="text"
           onChange={handleChange}
           value={value}
           onFocus={activateField}
           onBlur={deactivateField}
           className={`${[
-            space['p--0'],
-            shape['w--full'],
-            layout['block'],
-            color['b--0'],
-            font['size--16'],
-            font['weight--300'],
-            color['c__placeholder--black'],
-            input['input'],
-          ].join(' ')} ${styleInput(errors, fieldActive, checkEmail(value))}`}
-          placeholder={fieldActive ? 'Email' : undefined}
+            space["p--0"],
+            shape["w--full"],
+            layout["block"],
+            color["b--0"],
+            font["size--16"],
+            font["weight--300"],
+            color["c__placeholder--black"],
+            input["input"],
+          ].join(" ")} ${styleInput(errors, fieldActive, checkEmail(value))}`}
+          placeholder={fieldActive ? "Email" : undefined}
         />
         <label
-          htmlFor='email'
+          htmlFor="email"
           className={`${[
-            layout['absolute'],
-            font['size--12'],
-            color['c--gray__1'],
-            font['weight--100'],
-            input['label'],
-          ].join(' ')} ${styleLabel(
+            layout["absolute"],
+            font["size--12"],
+            color["c--gray__1"],
+            font["weight--100"],
+            input["label"],
+          ].join(" ")} ${styleLabel(
             errors,
             fieldActive,
             checkEmail(value),
             value
-          )}`}>
+          )}`}
+        >
           Email
         </label>
       </div>
