a044d472085b173a6efd98f987869d59a1da70b9
Kenichi Okiebisu <a01056715@gmail.com>
2020-12-20 00:52:25 -0800

    refactor: added path alias for bullet

--- client/components/atoms/bullet/bullet.amenity.tsx
+++ client/components/atoms/bullet/bullet.amenity.tsx
@@ -3,8 +3,8 @@ import React from "react";
 /** styles */
 import layout from "@styles/layout.module.scss";
 import space from "@styles/space.module.scss";
-import shape from "../../../styles/shape.module.scss";
-import font from "../../../styles/font.module.scss";
+import shape from "@styles/shape.module.scss";
+import font from "@styles/font.module.scss";
 
 /** vectors */
 import {
@@ -14,7 +14,7 @@ import {
   SmokeAlarm,
   Heating,
   TV,
-} from "../../../public/svg/original";
+} from "@svg/original";
 
 /**
  * Renders the amenity bullet
--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,17 @@
+const path = require("path");
 module.exports = {
-  distDir: 'build',
+  distDir: "build",
+  webpack(config, options) {
+    config.resolve.alias["@atoms"] = path.join(__dirname, "components/atoms/*");
+    config.resolve.alias["@molecules"] = path.join(
+      __dirname,
+      "components/molecules/*"
+    );
+    config.resolve.alias["@organisms"] = path.join(
+      __dirname,
+      "components/organisms/*"
+    );
+    config.resolve.alias["@svg"] = path.join(__dirname, "public/svg");
+    return config;
+  },
 };
