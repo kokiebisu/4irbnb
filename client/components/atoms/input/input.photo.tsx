6df6cb3c28f12695a5dd1c8acb867dadec99db05
Kenichi Okiebisu <a01056715@gmail.com>
2020-12-20 19:52:09 -0800

    feat: styled photo input

--- client/components/atoms/input/input.photo.tsx
+++ client/components/atoms/input/input.photo.tsx
@@ -8,6 +8,8 @@ import layout from "@styles/layout.module.scss";
 import font from "@styles/font.module.scss";
 import input from "@input/input.module.scss";
 
+import { Upload } from "@svg/original";
+
 /**
  * Renders the text input component
  * @param {string} name - Type of input
@@ -25,82 +27,86 @@ export const PhotoInput: React.FC<{
   return (
     <div>
       <form>
-        <div>
-          <input type="file" />
-        </div>
-        <div
-          style={{
-            maxWidth: 440,
-            height: 350,
-            padding: 15,
-            border: "1px dashed lightgray",
-          }}
-        >
+        <input type="file" id="upload" style={{ display: "none" }} />
+        <label htmlFor="upload">
           <div
             style={{
-              position: "relative",
-              width: "100%",
-              height: "100%",
-              backgroundImage:
-                "url(https://a0.muscache.com/airbnb/static/packages/upload-photos-ghosts.56fe723d.png)",
+              cursor: "pointer",
+              maxWidth: 440,
+              height: 350,
+              padding: 15,
+              border: "1px dashed lightgray",
             }}
           >
             <div
               style={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                backgroundImage:
+                  "url(https://a0.muscache.com/airbnb/static/packages/upload-photos-ghosts.56fe723d.png)",
               }}
             >
               <div
                 style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
                 }}
               >
-                <div>
-                  <div
-                    style={{
-                      padding: 14,
-                      backgroundColor: "#FF5A5F",
-                      borderRadius: 4,
-                    }}
-                  >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <div>
                     <div
-                      className={[layout["flex"], layout["items-center"]].join(
-                        " "
-                      )}
+                      style={{
+                        padding: "14px 22px",
+                        backgroundColor: "#FF5A5F",
+                        borderRadius: 4,
+                      }}
                     >
-                      <div></div>
-                      <div>
-                        <h3
-                          style={{
-                            fontSize: 14,
-                            whiteSpace: "nowrap",
-                            color: "white",
-                          }}
-                        >
-                          Upload Photos
-                        </h3>
+                      <div
+                        className={[
+                          layout["flex"],
+                          layout["items-center"],
+                        ].join(" ")}
+                      >
+                        <div className={[space["m-r--8"]].join(" ")}>
+                          <Upload width={20} stroke="white" />
+                        </div>
+                        <div>
+                          <h3
+                            style={{
+                              fontSize: 14,
+                              whiteSpace: "nowrap",
+                              color: "white",
+                            }}
+                          >
+                            Upload Photos
+                          </h3>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className={[space["m-t--8"]].join(" ")}>
-                  <h4
-                    className={[font["size--15"], color["c--gray__0"]].join(
-                      " "
-                    )}
-                  >
-                    or drag them in
-                  </h4>
+                  <div className={[space["m-t--8"]].join(" ")}>
+                    <h4
+                      className={[font["size--15"], color["c--gray__0"]].join(
+                        " "
+                      )}
+                    >
+                      or drag them in
+                    </h4>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </label>
       </form>
     </div>
   );
