a36c8511b7b8196f01543a0b6699b4e2d0e82cfb
Kenichi Okiebisu <a01056715@gmail.com>
2020-12-20 18:18:00 -0800

    feat: created basic structure of price page

--- client/components/organisms/create/create.price.tsx
+++ client/components/organisms/create/create.price.tsx
@@ -36,11 +36,11 @@ export const PriceCreate: React.FC<{ setData?: any; data?: any }> = ({
             </h4>
           </div>
         </div>
-        <div>
+        <div className={[space["m-b--24"]].join(" ")}>
           <div className={[layout["flex"], layout["items-center"]].join(" ")}>
             <div className={[space["m-r--4"]].join(" ")}>
               <h2 className={[font["size--16"]].join(" ")}>
-                Set up Smart Pricing
+                Set up the same base price for each night
               </h2>
             </div>
             <div
@@ -68,25 +68,42 @@ export const PriceCreate: React.FC<{ setData?: any; data?: any }> = ({
             </p>
           </div>
         </div>
-        <div>
-          <Input
-            variant="price"
-            title="Base price"
-            subtitle="This will be your default price gor days when you decide to turn off Smart Pricing."
-            tip={17}
-            name="basePrice"
-            value={data.basePrice}
-            handleChange={(e) => {
-              const onlyNumbers = /^[0-9\b]+$/;
-              if (e.target.value == "" || onlyNumbers.test(e.target.value)) {
-                setData({ ...data, basePrice: e.target.value });
-              }
-            }}
-          />
+        <div className={[space["m-b--24"]].join(" ")}>
+          <div>
+            <h3 className={[font["size--14"]].join(" ")}>Base price</h3>
+          </div>
+          <div className={[space["m-b--8"]].join(" ")}>
+            <p className={[font["size--13"]].join(" ")}>
+              This will be your default price.
+            </p>
+          </div>
+          <div>
+            <Input
+              variant="price"
+              title="Base price"
+              subtitle="This will be your default price gor days when you decide to turn off Smart Pricing."
+              tip={17}
+              name="basePrice"
+              value={data.basePrice}
+              handleChange={(e) => {
+                const onlyNumbers = /^[0-9\b]+$/;
+                if (e.target.value == "" || onlyNumbers.test(e.target.value)) {
+                  setData({ ...data, basePrice: e.target.value });
+                }
+              }}
+            />
+          </div>
+          <div className={[space["m-v--8"]].join(" ")}>
+            <h4
+              className={[color["c--darkgreen__3"], font["size--16"]].join(" ")}
+            >
+              Tip: $21
+            </h4>
+          </div>
         </div>
         <div>
-          <div>
-            <h3>Currency</h3>
+          <div className={[space["m-b--8"]].join(" ")}>
+            <h3 className={[font["size--14"]].join(" ")}>Currency</h3>
           </div>
           <div>
             <Input variant="select" inputType="currency" />
