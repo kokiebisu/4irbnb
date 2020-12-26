6cc8311fd2217f1848786aab76b06ed8a977cc0a
Kenichi Okiebisu <a01056715@gmail.com>
2020-12-26 15:48:28 -0800

    created global decorators

--- client/components/organisms/header/header.stories.tsx
+++ client/components/organisms/header/header.stories.tsx
@@ -7,13 +7,6 @@ export default {
   title: "Design System/Organisms/Header",
   component: Header,
   argTypes: { onClick: { action: "clicked" } },
-  decorators: [
-    (Story) => (
-      <ContextProvider>
-        <Story />
-      </ContextProvider>
-    ),
-  ],
 } as Meta;
 
 const TemplateStory: Story<HeaderProps> = (args) => <Header {...args} />;
