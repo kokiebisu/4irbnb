f148fb6a0e2975e9de7f2d495f64fd2b63c22312
Kenichi Okiebisu <a01056715@gmail.com>
2020-12-29 01:30:41 -0800

    refactor: updated dockerfile for services

---
+++ packages/error/src/notFound.ts
@@ -0,0 +1,14 @@
+import { CustomError } from "./custom";
+
+export class NotFoundError extends CustomError {
+  statusCode = 404;
+
+  constructor() {
+    super("Page was not found");
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
+
+  serializeErrors = () => {
+    return [{ message: "Page not found" }];
+  };
+}
