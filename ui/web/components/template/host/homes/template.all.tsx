5e682caa7c009db370a75dc5bdd9ee0dfe7f9bbf
Kenichi Okiebisu <a01056715@gmail.com>
2021-01-01 22:19:07 -0800

    fix: minor issues

--- ui/web/components/template/host/homes/template.all.tsx
+++ ui/web/components/template/host/homes/template.all.tsx
@@ -5,39 +5,11 @@ import { Layout } from "@layout";
 /**
  * Renders the All section
  */
-export const AllTemplate: React.FC<{}> = () => {
-  const stayTypes = [
-    {
-      name: "Houses",
-      imgUrl:
-        "https://a0.muscache.com/im/pictures/ccef24a3-d378-48c7-ad1a-ee83d313fef5.jpg?im_q=highq&im_w=320",
-    },
-    {
-      name: "Tiny houses",
-      imgUrl:
-        "https://a0.muscache.com/im/pictures/8bbcea0d-05d5-4f26-953a-77cf80f1c5ee.jpg?im_q=highq&im_w=320",
-    },
-    {
-      name: "Guesthouses",
-      imgUrl:
-        "https://a0.muscache.com/im/pictures/1385145a-0d2d-43fa-b6c5-6c21944e723c.jpg?im_q=highq&im_w=320",
-    },
-    {
-      name: "Bed and breakfasts",
-      imgUrl:
-        "https://a0.muscache.com/im/pictures/812d72a8-2c76-4dc5-ba51-239d59d1ff5a.jpg?im_q=highq&im_w=320",
-    },
-    {
-      name: "Apartments",
-      imgUrl:
-        "https://a0.muscache.com/im/pictures/1976f418-6b52-4c24-a77f-f7ee2211e0ed.jpg?im_q=highq&im_w=320",
-    },
-    {
-      name: "Boutique hotels",
-      imgUrl:
-        "https://a0.muscache.com/im/pictures/980c9ff9-3e32-4f93-9771-bd39b20b27f4.jpg?im_q=highq&im_w=320",
-    },
-  ];
+export const AllTemplate: React.FC<{
+  items?: { name: string; imgUrl: string }[];
+}> = ({
+  items = [undefined, undefined, undefined, undefined, undefined, undefined],
+}) => {
   return (
     <Layout variant="homes" title="From castles to condos, guests book it all">
       <div
@@ -49,13 +21,13 @@ export const AllTemplate: React.FC<{}> = () => {
           scrollSnapType: "x mandatory",
         }}
       >
-        {stayTypes.map((stayType, index) => {
+        {items.map((item, index) => {
           return (
             <div key={index} style={{ scrollSnapAlign: "start" }}>
               <Card
                 variant="category"
-                imgUrl={stayType.imgUrl}
-                title={stayType.name}
+                imgUrl={item?.imgUrl}
+                title={item?.name}
               />
             </div>
           );
