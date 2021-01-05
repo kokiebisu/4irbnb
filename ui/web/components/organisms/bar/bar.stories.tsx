dfb105ce8a56c728dd245d9cf5116076b6ba1a38
Kenichi Okiebisu <a01056715@gmail.com>
2021-01-05 00:41:36 -0800

    refactor: resolve error

--- ui/web/components/organisms/bar/bar.stories.tsx
+++ ui/web/components/organisms/bar/bar.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
-import { Bar, BarProps } from "@bar/";
+import { Bar, BarProps } from "@bar";
 
 export default {
   title: "Organisms/Bar",
