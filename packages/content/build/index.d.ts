de5c489570f7c74b8dabd9c73857d369a43dff27
Kenichi Okiebisu <a01056715@gmail.com>
2021-01-05 15:28:02 -0800

    refactor: created content package

---
+++ packages/content/build/index.d.ts
@@ -0,0 +1,27 @@
+export declare const categories: {
+    imgUrl: string;
+    title: string;
+    to: string;
+}[];
+export declare const anywhere: {
+    imgUrl: string;
+    title: string;
+    to: string;
+}[];
+export declare const sections: {
+    name: string;
+    items: {
+        url: string;
+        name: string;
+    }[];
+}[];
+export declare const nearbyItems: {
+    imgUrl: string;
+    hours: number;
+    city: string;
+    to: string;
+}[];
+export declare const onlines: {
+    imgUrl: string;
+    title: string;
+}[];
