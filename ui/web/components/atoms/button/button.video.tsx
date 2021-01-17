c40fb46abbdbdbb2979efaf8ac2a7737d3f88730
Kenichi Okiebisu <a01056715@gmail.com>
2021-01-16 19:31:59 -0800

    fix: added hover animation to video button

--- ui/web/components/atoms/button/button.video.tsx
+++ ui/web/components/atoms/button/button.video.tsx
@@ -1,10 +1,17 @@
 import { Pause, Play } from "@svg/regular";
+import space from "@styles/space.module.scss";
 import shape from "@styles/shape.module.scss";
 import animation from "@styles/animation.module.scss";
 
 export const VideoButton = ({ play = false }) => {
   return (
-    <div className={[shape["br--circle"], animation["hover--blur"]].join(" ")}>
+    <div
+      className={[
+        shape["br--circle"],
+        animation["hover-blur"],
+        space["p--8"],
+      ].join(" ")}
+    >
       {play ? (
         <Play width={15} height={15} />
       ) : (
