a2a2ef50aee00faf0444845c680e9acb1fe888f3
Kenichi Okiebisu <a01056715@gmail.com>
2021-01-16 23:56:46 -0800

    feat: attached filter component to online experiences page

--- ui/web/pages/s/experiences/online.tsx
+++ ui/web/pages/s/experiences/online.tsx
@@ -5,6 +5,7 @@ import { Template } from "@template/s/experiences/online";
 import layout from "@styles/layout.module.scss";
 import space from "@styles/space.module.scss";
 import shape from "@styles/shape.module.scss";
+import color from "@styles/color.module.scss";
 
 const OnlinePage = () => {
   return (
@@ -17,9 +18,25 @@ const OnlinePage = () => {
       <div className={[layout["container--spread"], space["m-v--8"]].join(" ")}>
         <Template variant="banner" />
       </div>
-      <div className={[layout["container--spread"], space["m-y--8"]].join(" ")}>
-        <Bar variant="filters" />
+      <div style={{ position: "sticky", top: 0, zIndex: 50 }}>
+        <div className={[color["bg--white"]].join(" ")}>
+          <div
+            className={[layout["container--spread"], space["p-v--16"]].join(
+              " "
+            )}
+          >
+            <Bar variant="filters" />
+          </div>
+          <div
+            className={[color["bg--white__2"]].join(" ")}
+            style={{
+              width: "100%",
+              height: 1,
+            }}
+          />
+        </div>
       </div>
+
       <div className={[layout["container--spread"], space["m-y--8"]].join(" ")}>
         <Template variant="cards" />
       </div>
