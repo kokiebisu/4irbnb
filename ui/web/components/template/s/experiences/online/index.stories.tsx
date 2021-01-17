1c73327904e8f325c29327b4e31578af1d291b26
Kenichi Okiebisu <a01056715@gmail.com>
2021-01-16 21:13:34 -0800

    feat: created basic structure of cards template

---
+++ ui/web/components/template/s/experiences/online/index.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { Template, TemplateProps } from "@template/s/experiences/online";
+
+export default {
+  title: "Templates/S/Experiences/Online",
+  component: Template,
+  argTypes: {
+    onClick: { action: "clicked" },
+    type: { control: { disable: true } },
+    extendsTo: { control: { disable: true } },
+  },
+} as Meta;
+
+const TemplateStory: Story<TemplateProps> = (args) => <Template {...args} />;
+
+export const Banner = TemplateStory.bind({});
+Banner.args = {
+  variant: "banner",
+};
+
+export const Cards = TemplateStory.bind({});
+Cards.args = {
+  variant: "cards",
+};
---
+++ ui/web/components/template/s/experiences/online/index.tsx
@@ -0,0 +1,33 @@
+/** landing */
+import { BannerTemplate } from "@template/s/experiences/online/template.banner";
+import { CardsTemplate } from "./template.cards";
+
+export interface TemplateProps {
+  extendsTo?: string;
+  variant?: string;
+  [property: string]: any;
+}
+
+/**
+ * Bundles the button components
+ * @param {string} extendsTo - Add custom styling to the specified component
+ * @param {string} type - Specifies the type of button component
+ * @param {Object} children - A JSX that will be part of the component
+ */
+export const Template: React.FC<TemplateProps> = ({
+  extendsTo = "",
+  variant = "banner",
+  children,
+  onClick,
+  to,
+  block,
+  animate,
+  ...props
+}) => {
+  const variants: { [key: string]: JSX.Element } = {
+    banner: <BannerTemplate {...props} />,
+    cards: <CardsTemplate {...props} />,
+  };
+
+  return <div data-testid={`${variant}--template`}>{variants[variant]}</div>;
+};
