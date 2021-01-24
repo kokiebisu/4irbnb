66572217f16bbecb688a9ccd86f2c6a609594910
Kenichi Okiebisu <a01056715@gmail.com>
2021-01-23 22:32:44 -0800

    created button variant file

--- ui/web/components/atoms/button/__tests__/button.auth.test.tsx
+++ ui/web/components/atoms/button/__tests__/button.auth.test.tsx
@@ -3,10 +3,12 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { Button } from "@button";
 
+import * as variant from "@button/variants";
+
 describe("auth button", () => {
   it("renders correctly", () => {
     const { getByTestId } = render(
-      <Button variant="auth" onClick={() => console.log("clicked")} />
+      <Button variant={variant.AUTH} onClick={() => console.log("clicked")} />
     );
     expect(getByTestId("auth-button--atom")).toHaveTextContent(
       "Continue with Email"
@@ -14,7 +16,7 @@ describe("auth button", () => {
   });
   it("calls onClick prop when clicked", () => {
     const handleClick = jest.fn();
-    render(<Button variant="auth" onClick={handleClick} />);
+    render(<Button variant={variant.AUTH} onClick={handleClick} />);
     fireEvent.click(screen.getByText(/Continue with Email/i));
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
