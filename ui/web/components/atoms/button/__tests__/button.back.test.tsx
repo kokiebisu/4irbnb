f8c1c518e8d81bbab7b505d3f123d9f421df21ae
Kenichi Okiebisu <a01056715@gmail.com>
2021-01-23 22:32:44 -0800

    created button variant file

--- ui/web/components/atoms/button/__tests__/button.back.test.tsx
+++ ui/web/components/atoms/button/__tests__/button.back.test.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import { screen, fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { Button } from "..";
-import { Back } from "@button/button.stories";
+import * as variant from "@button/variants";
 
 describe("back button", () => {
   it("renders correctly", () => {
     const { getByTestId } = render(
-      <Button variant="back" onClick={() => console.log("clicked")} />
+      <Button variant={variant.BACK} onClick={() => console.log("clicked")} />
     );
     expect(getByTestId("back-button--atom")).toHaveTextContent("Back");
   });
   it("calls onClick prop when clicked", () => {
     const handleClick = jest.fn();
-    render(<Button onClick={handleClick} {...Back.args} />);
+    render(<Button onClick={handleClick} variant={variant.BACK} />);
     fireEvent.click(screen.getByText(/Back/i));
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
--- ui/web/components/atoms/button/__tests__/button.banner.test.tsx
+++ ui/web/components/atoms/button/__tests__/button.banner.test.tsx
@@ -2,30 +2,31 @@ import React from "react";
 import { screen, fireEvent, render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { Button } from "@button";
+import * as variant from "@button/variants";
 
 afterEach(cleanup);
 
 describe("banner button", () => {
   it("renders correctly", () => {
     const { getByTestId } = render(
-      <Button variant="banner" onClick={() => console.log("clicked")} />
+      <Button variant={variant.BANNER} onClick={() => console.log("clicked")} />
     );
     expect(getByTestId("banner-button--atom")).toHaveTextContent("Button");
   });
 
   it("calls onClick prop when clicked", () => {
     const handleClick = jest.fn();
-    render(<Button variant="banner" onClick={handleClick} />);
+    render(<Button variant={variant.BANNER} onClick={handleClick} />);
     fireEvent.click(screen.getByText(/Button/i));
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
 
   it("rerenders correctly with prop change", () => {
     const { getByText, rerender } = render(
-      <Button variant="banner" title="Previous" />
+      <Button variant={variant.BANNER} title="Previous" />
     );
     getByText("Previous");
-    rerender(<Button variant="banner" title="After" />);
+    rerender(<Button variant={variant.BANNER} title="After" />);
     getByText("After");
   });
 });
