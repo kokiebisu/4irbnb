0e1c409e84439663c96aa451de1efc9331f7484e
Kenichi Okiebisu <a01056715@gmail.com>
2021-01-28 19:40:40 -0800

    fix: circleci config

--- packages/content/build/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./src/all";
-export * from "./src/anywhere";
-export * from "./src/category";
-export * from "./src/footer";
-export * from "./src/hosts";
-export * from "./src/destination";
--- packages/content/build/src/category.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const categoryItems: {
-    imgUrl: string;
-    title: string;
-    to: string;
-}[];
