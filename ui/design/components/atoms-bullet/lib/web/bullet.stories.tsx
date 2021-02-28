0f5961f433b10e8d8e46f69937f99aaa975a149f
Kenichi Okiebisu <a01056715@gmail.com>
2021-02-28 09:37:33 -0800

    fix: atoms-bullet storybook

--- ui/design/components/atoms-bullet/lib/web/bullet.stories.tsx
+++ ui/design/components/atoms-bullet/lib/web/bullet.stories.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { Story, Meta } from "@storybook/react";
-import { Bullet, BulletProps, $Bullet } from "..";
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { Bullet, BulletProps, $BULLET } from '..';
 
 export default {
-  title: "Atoms/Bullet",
+  title: 'Atoms/Bullet',
   component: Bullet,
   argTypes: {
     variant: {
@@ -19,53 +19,53 @@ const TemplateStory: Story<BulletProps> = (args) => <Bullet {...args} />;
 
 export const Characteristic = TemplateStory.bind({});
 Characteristic.args = {
-  variant: $Bullet.CHARACTERISTIC,
+  variant: $BULLET.characteristic,
 };
 Characteristic.argTypes = {
   characteristicType: {
     control: {
-      type: "select",
-      options: ["house", "sparkle", "door", "calendar", "guidelines", "time"],
+      type: 'select',
+      options: ['house', 'sparkle', 'door', 'calendar', 'guidelines', 'time'],
     },
   },
 };
 
 export const Amenity = TemplateStory.bind({});
 Amenity.args = {
-  variant: $Bullet.AMENITY,
+  variant: $BULLET.amenity,
 };
 Amenity.argTypes = {
   removed: {
-    control: "boolean",
+    control: 'boolean',
   },
   amenityType: {
     control: {
-      type: "select",
-      options: ["smoke", "tv", "kitchen", "heating", "entrance", "carbon"],
+      type: 'select',
+      options: ['smoke', 'tv', 'kitchen', 'heating', 'entrance', 'carbon'],
     },
   },
 };
 
 export const Know = TemplateStory.bind({});
 Know.args = {
-  variant: $Bullet.KNOW,
+  variant: $BULLET.know,
 };
 Know.argTypes = {
   categoryType: {
     control: {
-      type: "select",
+      type: 'select',
       options: [
-        "checkin",
-        "checkout",
-        "self",
-        "children",
-        "smoking",
-        "pets",
-        "parties",
-        "cleaning",
-        "distancing",
-        "caution",
-        "check",
+        'checkin',
+        'checkout',
+        'self',
+        'children',
+        'smoking',
+        'pets',
+        'parties',
+        'cleaning',
+        'distancing',
+        'caution',
+        'check',
       ],
     },
   },
@@ -73,74 +73,74 @@ Know.argTypes = {
 
 export const Rating = TemplateStory.bind({});
 Rating.args = {
-  variant: $Bullet.RATING,
+  variant: $BULLET.rating,
 };
 
 export const Bring = TemplateStory.bind({});
 Bring.args = {
-  variant: $Bullet.BRING,
+  variant: $BULLET.bring,
 };
 
 export const Experience = TemplateStory.bind({});
 Experience.args = {
-  variant: $Bullet.EXPERIENCE,
+  variant: $BULLET.experience,
 };
 Experience.argTypes = {
   experience: {
     control: {
-      type: "select",
-      options: ["host", "activities", "global"],
+      type: 'select',
+      options: ['host', 'activities', 'global'],
     },
   },
 };
 
 export const Priority = TemplateStory.bind({});
 Priority.args = {
-  variant: $Bullet.PRIORITY,
+  variant: $BULLET.priority,
 };
 Priority.argTypes = {
   priority: {
     control: {
-      type: "select",
-      options: ["protection", "guidance", "requirements"],
+      type: 'select',
+      options: ['protection', 'guidance', 'requirements'],
     },
   },
 };
 
 export const Help = TemplateStory.bind({});
 Help.args = {
-  variant: $Bullet.HELP,
+  variant: $BULLET.help,
 };
 Help.argTypes = {
   help: {
     control: {
-      type: "select",
-      options: ["support", "tools", "insights", "education"],
+      type: 'select',
+      options: ['support', 'tools', 'insights', 'education'],
     },
   },
 };
 
 export const Required = TemplateStory.bind({});
 Required.args = {
-  variant: $Bullet.REQUIRED,
+  variant: $BULLET.required,
 };
 
 export const OnlineHost = TemplateStory.bind({});
 OnlineHost.args = {
-  variant: $Bullet.ONLINEHOST,
+  variant: $BULLET.onlinehost,
 };
 
 export const Score = TemplateStory.bind({});
 Score.args = {
-  variant: $Bullet.SCORE,
+  variant: $BULLET.score,
 };
 
 export const Question = TemplateStory.bind({});
 Question.args = {
-  variant: $Bullet.QUESTION,
+  variant: $BULLET.question,
 };
 
 export const Check = TemplateStory.bind({});
 Check.args = {
-  variant: $Bullet.CHECK,
+  variant: $BULLET.check,
 };
