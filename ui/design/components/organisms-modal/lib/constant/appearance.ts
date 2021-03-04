25f42db4b5866c0ac3c4181ddac072d40cc5d72f
Kenichi Okiebisu <a01056715@gmail.com>
2021-03-03 21:07:32 -0800

    feat: created appearance file in organisms-modal

---
+++ ui/design/components/organisms-modal/lib/constant/appearance.ts
@@ -0,0 +1,12 @@
+export const $MODAL = {
+  privacy: 'privacy',
+  menu: 'menu',
+  auth: 'auth',
+  booking: 'booking',
+  globe: 'globe',
+  location: 'location',
+  guests: 'guests',
+  checkin: 'checkin',
+  checkout: 'checkout',
+  listing: 'listing',
+};
