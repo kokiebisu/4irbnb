14b089b0b516d4b26e2e6ba7cd82732e37132261
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-03 14:20:23 -0700

    refactor: templates/s/homes also segment

--- packages/web/components/template/s/homes/also/template.tsx
+++ packages/web/components/template/s/homes/also/template.tsx
@@ -1,58 +1,45 @@
-import { useRef } from "react";
+import { useRef } from 'react';
 
-import { Card, $Card } from "@card";
-import { Button, $Button } from "@button";
+import { Card, $Card } from '@card';
+import { Button, $Button } from '@button';
 
-import layout from "@styles/layout.module.scss";
-import font from "@styles/font.module.scss";
-import space from "@styles/space.module.scss";
-import section from "@template/index.module.scss";
+import layout from '@styles/layout.module.scss';
+import font from '@styles/font.module.scss';
+import space from '@styles/space.module.scss';
+import section from '@template/index.module.scss';
 
-import * as Helpers from "@helper/array";
+import { useSlider } from '@hooks/useSlider';
+import { useHandleContainerResize } from '@hooks/useHandleContainerResize';
 
-import { useSlider } from "@hooks/useSlider";
-import { useHandleContainerResize } from "@hooks/useHandleContainerResize";
+export interface AlsoTemplateProps {
+  items?: any;
+  title?: string;
+}
 
 /**
  * Renders the also section
  * @param {string[]} items - List of suggested cities
  */
-export const AlsoTemplate: React.FC<{
-  items?: any;
-  title?: string;
-}> = ({
-  items = [
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-    undefined,
-  ],
-  title = "Section title",
-}) => {
+export const AlsoTemplate: React.FC<AlsoTemplateProps> = ({ items, title }) => {
   const containerRef = useRef<HTMLDivElement>();
   const width: number = useHandleContainerResize(containerRef)[0];
-  const { state, previous, next } = useSlider(items, width, "also");
-  const displayingItems = Helpers.groupByTwo(items);
+  const { state, previous, next } = useSlider(items, width, 'also');
 
   return (
-    <div style={{ overflowX: "hidden" }}>
+    <div style={{ overflowX: 'hidden' }}>
       <div
         ref={containerRef}
         className={[
-          layout["flex"],
-          layout["items-center"],
-          layout["justify-between"],
-        ].join(" ")}
+          layout['flex'],
+          layout['items-center'],
+          layout['justify-between'],
+        ].join(' ')}
       >
-        <div className={[space["m-v--16"]].join(" ")}>
-          <h3 className={[font["size--20"]].join(" ")}>{title}</h3>
+        <div className={[space['m-v--16']].join(' ')}>
+          <h3 className={[font['size--20']].join(' ')}>{title}</h3>
         </div>
-        <div className={[layout["flex"], layout["items-center"]].join(" ")}>
-          <div className={[space["m-h--2"]].join(" ")}>
+        <div className={[layout['flex'], layout['items-center']].join(' ')}>
+          <div className={[space['m-h--2']].join(' ')}>
             <Button
               variant={$Button.PAGINATE}
               animate
@@ -61,7 +48,7 @@ export const AlsoTemplate: React.FC<{
               disable={state.activeSlide === 0}
             />
           </div>
-          <div className={[space["m-h--2"]].join(" ")}>
+          <div className={[space['m-h--2']].join(' ')}>
             <Button
               variant={$Button.PAGINATE}
               animate
@@ -76,26 +63,26 @@ export const AlsoTemplate: React.FC<{
       </div>
       <div
         style={{
-          height: "100%",
+          height: '100%',
           width: width * (items.length / 2),
           transform: `translateX(-${state.translate}px)`,
           transition: `transform ease-out ${state.transition}s`,
         }}
       >
-        <div style={{ display: "flex" }}>
+        <div style={{ display: 'flex' }}>
           {displayingItems.map((item, index) => {
             return (
               <div
                 style={{ width: width / (width > 728 ? 3 : 2) }}
                 key={index}
-                className={[section["w__nearby"]].join(" ")}
+                className={[section['w__nearby']].join(' ')}
               >
-                <div className={[space["m-b--10"]].join(" ")}>
+                <div className={[space['m-b--10']].join(' ')}>
                   <Card
                     variant={$Card.NEARBY}
                     to={item[0]?.to}
                     imgUrl={item?.pic}
-                    title={item[0]?.city || "Item"}
+                    title={item[0]?.city || 'Item'}
                     subtitle={`${item[0]?.hours || 1} hour drive`}
                     size="lg"
                   />
@@ -105,7 +92,7 @@ export const AlsoTemplate: React.FC<{
                     variant={$Card.NEARBY}
                     to={item[1]?.to}
                     imgUrl={item?.pic}
-                    title={item[1]?.city || "Item"}
+                    title={item[1]?.city || 'Item'}
                     subtitle={`${item[0]?.hours || 1} hour drive`}
                     size="lg"
                   />
