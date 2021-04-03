9c3ef9e1602eec5a708bf36a4663a2ba1671ca2e
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-02 20:13:02 -0700

    feat: experiences

---
+++ packages/web/components/template/experiences/id/experiences/template.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import {
+  ExperiencesTemplate,
+  ExperiencesTemplateProps,
+} from '@template/experiences/id/experiences/template';
+
+export default {
+  title: 'Templates/Experiences/ID',
+  component: ExperiencesTemplate,
+} as Meta;
+
+const TemplateStory: Story<ExperiencesTemplateProps> = (args) => (
+  <ExperiencesTemplate {...args} />
+);
+
+export const Experiences = TemplateStory.bind({});
+Experiences.args = {
+  experiences: ['hosts', 'activities', 'global'],
+};
