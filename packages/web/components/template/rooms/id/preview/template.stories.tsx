53a2d8726f0c061a1e06112daacfc31eb999ec8a
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-03 12:44:04 -0700

    refactor: template/rooms/id preview segment

---
+++ packages/web/components/template/rooms/id/preview/template.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+import { PreviewTemplate, PreviewTemplateProps } from './template';
+import * as mockData from './mock';
+
+export default {
+  title: 'Templates/Index',
+  component: PreviewTemplate,
+} as Meta;
+
+const TemplateStory: Story<PreviewTemplateProps> = (args) => (
+  <PreviewTemplate {...args} />
+);
+
+export const Preview = TemplateStory.bind({});
+Preview.args = {
+  ...mockData,
+};
