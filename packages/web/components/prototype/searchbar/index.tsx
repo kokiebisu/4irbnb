c1a8e0194bcce441efddd1f67fa7b4a881d05e66
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-07 22:18:30 -0700

    fix: searchbar

--- packages/web/components/prototype/searchbar/index.tsx
+++ packages/web/components/prototype/searchbar/index.tsx
@@ -1,15 +1,5 @@
-import { useRef, useState } from "react";
-
-import space from "@styles/space.module.scss";
-import shape from "@styles/shape.module.scss";
-import layout from "@styles/layout.module.scss";
-
-import { useToggleState } from "@context/toggle";
-
-import { Bar, $Bar } from "@bar";
-import { Modal } from "@modal";
-
-import { useOnClickOutside } from "@hooks/useOnClickOutside";
+import { useSearchbar } from './logic';
+import { SearchbarPrototypeTemplate } from './template';
 
 export interface PrototypeProps {
   expanded?: boolean;
@@ -26,82 +16,16 @@ export const Prototype: React.FC<PrototypeProps> = ({
   expanded,
   setExpanded,
 }) => {
-  const toggleState = useToggleState();
-  const [selected, setSelected] = useState(null);
-  const containerRef = useRef();
-
-  useOnClickOutside(containerRef, () => {
-    if (selected) {
-      setSelected(!selected);
-    }
-    if (expanded) {
-      setExpanded(!expanded);
-    }
-  });
-
-  const contents = {
-    location: {
-      styles: {
-        left: 0,
-        maxWidth: 400,
-      },
-    },
-    checkin: {
-      styles: {
-        left: 0,
-        maxWidth: 780,
-      },
-    },
-    checkout: {
-      styles: {
-        left: 0,
-        maxWidth: 780,
-      },
-    },
-    guests: {
-      styles: {
-        right: 0,
-        maxWidth: 325,
-        display: "flex",
-        justifyContent: "flex-end",
-      },
-    },
-  };
+  const data = useSearchbar();
 
   return (
-    <div className={layout["relative"]}>
-      <Bar
-        variant={$Bar.SEARCH}
-        selected={selected}
-        setSelected={setSelected}
-        type={type}
-        setCategory={setCategory}
-        extendsTo={[space["p-h--12"]].join(" ")}
-        transparent={transparent}
-      />
-      {Object.keys(contents).map((content, index) => {
-        return (
-          <div
-            key={index}
-            style={{
-              position: "absolute",
-              top: 60,
-              zIndex: 80,
-              width: "100%",
-              ...contents[content].styles,
-            }}
-          >
-            <div className={[shape["w--full"]].join(" ")} ref={containerRef}>
-              <Modal
-                variant={content}
-                dispatch={`toggle_${content}`}
-                extendsTo={[shape["w--full"]].join(" ")}
-                criteria={toggleState[content]}
-              />
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <SearchbarPrototypeTemplate
+      type={type}
+      setCategory={setCategory}
+      transparent={transparent}
+      expanded={expanded}
+      setExpanded={setExpanded}
+      {...data}
+    />
   );
 };
