1046e890ea4e9284a3c5b7fcdd709dc0b9ec13f0
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-09 21:50:19 -0700

    feat: segment checkin

---
+++ packages/web/components/segments/become-a-host/checkin/logic.ts
@@ -0,0 +1,37 @@
+import { useStayDispatch, useStayState } from '@context/stay';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import * as mockData from './mock';
+
+export const useCheckInSegment = () => {
+  const router = useRouter();
+  const stayDispatch = useStayDispatch();
+  const { notice, checkinFrom, checkinTo } = useStayState();
+  const [data, setData] = useState({
+    notice,
+    checkinFrom,
+    checkinTo,
+  });
+
+  const handleRedirectToNextPage = () => {
+    stayDispatch({
+      type: 'add',
+      payload: data,
+    });
+    setTimeout(() => {
+      router.push('/become-a-host/advance');
+    }, 500);
+  };
+
+  const handleRedirectToPreviousPage = () => {
+    setTimeout(() => {
+      router.push('/become-a-host/review-how-guests-book');
+    }, 500);
+  };
+
+  return {
+    ...mockData,
+    handleRedirectToNextPage,
+    handleRedirectToPreviousPage,
+  };
+};
