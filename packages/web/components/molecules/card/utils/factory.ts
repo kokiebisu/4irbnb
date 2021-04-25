17abdb427bdc1f1e56466656fce2edc052926988
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-24 17:28:43 -0700

    feat: card how

--- packages/web/components/molecules/card/utils/factory.ts
+++ packages/web/components/molecules/card/utils/factory.ts
@@ -10,7 +10,7 @@ import { collection } from '@card/card-collection';
 import { horizontal } from '@card/card-horizontal';
 import { host } from '@card/card-host';
 import { hosting } from '@card/card-hosting';
-import { how } from '@card/card.how';
+import { how } from '@card/card-how';
 import { online } from '@card/card.online';
 import { participate } from '@card/card.participate';
 import { review } from '@card/card.review';
