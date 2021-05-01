85c7834c57a8e9da28043ec0c67ac35f52686a8a
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-30 21:33:59 -0700

    fix: logo button

--- packages/web/components/atoms/button/button-logo/index.tsx
+++ packages/web/components/atoms/button/button-logo/index.tsx
@@ -1,25 +1,15 @@
-import { Icon } from '@icons';
+import { LogoButtonTemplate, LogoButtonTemplateProps } from './template';
 
-export interface LogoButtonProps {
-  noName?: boolean;
-  fill?: string;
-}
+export interface LogoButtonProps extends LogoButtonTemplateProps {}
 
-/**
- * Renders the Logo Button component
- * @param noName
- * @param fill
- */
-export const LogoButton: React.FC<LogoButtonProps> = ({ noName, fill }) => {
-  return noName ? (
-    <Icon variant="logo" logoType="noName" fill={fill} width={30} height={32} />
-  ) : (
-    <Icon variant="logo" logoType="name" fill={fill} width={102} height={32} />
-  );
+const LogoButton: React.FC<LogoButtonProps> = (props) => {
+  return <LogoButtonTemplate {...props} />;
 };
 
 export const logo = (props) => {
   return {
-    logo: <LogoButton {...props} />,
+    logo: {
+      component: <LogoButton {...props} />,
+    },
   };
 };
