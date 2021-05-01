5c04c4874aab881c7818e090dd597ef665c95109
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-30 22:02:43 -0700

    fix: report button

--- packages/web/components/atoms/button/button-report/index.tsx
+++ packages/web/components/atoms/button/button-report/index.tsx
@@ -1,25 +1,15 @@
-import { Icon } from '@icons';
+import { ReportButtonTemplate } from './template';
 
 export interface ReportButtonProps {}
 
-/**
- * Renders the Report Button
- */
-export const ReportButton: React.FC<ReportButtonProps> = () => {
-  return (
-    <div className="flex items-center">
-      <div className="mr-3">
-        <Icon variant="general" generalType="flag" width={16} />
-      </div>
-      <div>
-        <h5 className="font-light text-sm">Report this listing</h5>
-      </div>
-    </div>
-  );
+const ReportButton: React.FC<ReportButtonProps> = (props) => {
+  return <ReportButtonTemplate {...props} />;
 };
 
 export const report = (props) => {
   return {
-    report: <ReportButton {...props} />,
+    report: {
+      component: <ReportButton {...props} />,
+    },
   };
 };
