b587515b43555bff6cea9edf3073693bb4418712
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-30 20:56:45 -0700

    feat: closed button

--- packages/web/components/atoms/button/button-closed/index.tsx
+++ packages/web/components/atoms/button/button-closed/index.tsx
@@ -1,9 +1,25 @@
 import { ClosedButtonTemplate, ClosedButtonTemplateProps } from './template';
 
-export interface ClosedButtonProps extends ClosedButtonTemplateProps {}
+export interface ClosedButtonProps extends ClosedButtonTemplateProps {
+  closedType: 'close' | 'check';
+}
 
-const ClosedButton: React.FC<ClosedButtonProps> = ({ ...props }) => {
-  return <ClosedButtonTemplate {...props} />;
+const ClosedButton: React.FC<ClosedButtonProps> = ({
+  closedType,
+  ...props
+}) => {
+  const types = {
+    close: {
+      variant: 'action',
+      actionType: 'close',
+    },
+    check: {
+      variant: 'semantic',
+      semantictype: 'check',
+    },
+  };
+  const icon = types[closedType];
+  return <ClosedButtonTemplate {...props} {...icon} />;
 };
 
 export const closed = (props) => {
