ca6877224c5df2866b13bd44c030824da97bd3f5
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-30 22:25:25 -0700

    fix: video button

--- packages/web/components/atoms/button/button-video/index.tsx
+++ packages/web/components/atoms/button/button-video/index.tsx
@@ -1,35 +1,15 @@
-import { Icon } from '@icons';
+import { VideoButtonTemplate, VideoButtonTemplateProps } from './template';
 
-export interface VideoButtonProps {
-  play?: boolean;
-}
+export interface VideoButtonProps extends VideoButtonTemplateProps {}
 
-export const VideoButton: React.FC<VideoButtonProps> = ({ play }) => {
-  return (
-    <div className={`inline-block rounded-full p-3 hover:bg-blur`}>
-      {play ? (
-        <Icon
-          variant="action"
-          actionType="play"
-          width={15}
-          height={15}
-          fill="white"
-        />
-      ) : (
-        <Icon
-          variant="action"
-          actionType="pause"
-          width={15}
-          height={15}
-          fill="white"
-        />
-      )}
-    </div>
-  );
+const VideoButton: React.FC<VideoButtonProps> = (props) => {
+  return <VideoButtonTemplate />;
 };
 
 export const video = (props) => {
   return {
-    video: <VideoButton {...props} />,
+    video: {
+      component: <VideoButton {...props} />,
+    },
   };
 };
