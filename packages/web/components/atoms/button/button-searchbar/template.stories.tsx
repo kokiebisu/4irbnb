402d32e3c52d80b28d4fb5e5d4797ae2005799ab
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-30 22:13:59 -0700

    fix: searchbar button

---
+++ packages/web/components/atoms/button/button-searchbar/template.stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react';
+import {
+  SearchbarButtonTemplate,
+  SearchbarButtonTemplateProps,
+} from './template';
+
+export default {
+  title: 'Atoms/Button',
+  component: SearchbarButtonTemplate,
+} as Meta;
+
+const ButtonStory: Story<SearchbarButtonTemplateProps> = (args) => (
+  <SearchbarButtonTemplate {...args} />
+);
+
+export const Searchbar = ButtonStory.bind({});
+Searchbar.args = {
+  mini: false,
+  inverse: false,
+};
