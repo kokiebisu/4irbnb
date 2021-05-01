c8aa798ef117f939eb6549140907c8a55b1b3ab1
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-30 22:05:24 -0700

    fix: search button

--- packages/web/components/atoms/button/button-search/index.tsx
+++ packages/web/components/atoms/button/button-search/index.tsx
@@ -1,39 +1,9 @@
-import { Icon } from '@icons';
+import { SearchButtonTemplate, SearchButtonTemplateProps } from './template';
 
-export interface SearchButtonProps {
-  expand?: boolean;
-}
+export interface SearchButtonProps extends SearchButtonTemplateProps {}
 
-/**
- * Renders the Search Button component
- * @param {expand}
- */
-export const SearchButton: React.FC<SearchButtonProps> = ({ expand }) => {
-  return (
-    <div
-      className={`inline-flex items-center bg-primary rounded-full ${
-        expand ? 'px-5 py-3' : null
-      } transition`}
-    >
-      <div className={`${expand ? 'mr-1' : 'p-4'}`}>
-        <Icon
-          variant="general"
-          generalType="magnifyGlass"
-          width={13}
-          height={13}
-          stroke="white"
-          strokeWidth={4}
-        />
-      </div>
-      <div
-        className={`transition ${
-          expand ? `visible opacity-1` : `invisible opacity-0 w-0`
-        }`}
-      >
-        <p className="text-sm text-white">Search</p>
-      </div>
-    </div>
-  );
+const SearchButton: React.FC<SearchButtonProps> = ({ ...props }) => {
+  return <SearchButtonTemplate {...props} />;
 };
 
 export const search = (props) => {
