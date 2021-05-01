84e199226207dce5f1da87be3f064306bcada506
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-30 21:16:25 -0700

    fix: filter button

--- packages/web/components/atoms/button/button-filter/index.tsx
+++ packages/web/components/atoms/button/button-filter/index.tsx
@@ -1,31 +1,15 @@
-export interface FilterButtonProps {
-  label?: string;
-  inverse?: boolean;
-}
+import { FilterButtonTemplate } from './template';
 
-/**
- * Renders the filter button
- * @param {string} name - Name of the filter button
- */
-export const FilterButton: React.FC<FilterButtonProps> = ({
-  label,
-  inverse,
-}) => {
-  return (
-    <div
-      className={`inline-block whitespace-nowrap py-2 px-6 rounded-full ${
-        inverse
-          ? 'bg-transparent border-2 border-gray-300 hover:border-black'
-          : 'bg-gray-100 border-gray-100 border-2'
-      }`}
-    >
-      <p className="text-sm">{label}</p>
-    </div>
-  );
+export interface FilterButtonProps {}
+
+const FilterButton: React.FC<FilterButtonProps> = (props) => {
+  return <FilterButtonTemplate {...props} />;
 };
 
 export const filter = (props) => {
   return {
-    filter: <FilterButton {...props} />,
+    filter: {
+      component: <FilterButton {...props} />,
+    },
   };
 };
