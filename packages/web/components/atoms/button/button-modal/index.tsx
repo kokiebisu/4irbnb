82a89570d6dec1254bf02cae258d7956733541e8
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-30 21:43:57 -0700

    fix: modal button

--- packages/web/components/atoms/button/button-modal/index.tsx
+++ packages/web/components/atoms/button/button-modal/index.tsx
@@ -1,39 +1,9 @@
-import { Icon } from '@icons';
+import { ModalButtonTemplate, ModalButtonTemplateProps } from './template';
 
-export interface ModalButtonProps {
-  modalType?: 'close' | 'back';
-}
+export interface ModalButtonProps extends ModalButtonTemplateProps {}
 
-/**
- * Renders the close button component
- * @param {function} onClick - The action taken when the button is pressed
- */
-export const ModalButton: React.FC<ModalButtonProps> = ({ modalType }) => {
-  const types = {
-    close: (
-      <Icon
-        variant="action"
-        actionType="close"
-        width={16}
-        height={16}
-        stroke="black"
-      />
-    ),
-    back: (
-      <Icon
-        variant="action"
-        actionType="left"
-        width={16}
-        height={16}
-        stroke="black"
-      />
-    ),
-  };
-  return (
-    <div className="inline-block bg-transparent p-3 rounded-full hover:bg-gray-100">
-      {types[modalType]}
-    </div>
-  );
+const ModalButton: React.FC<ModalButtonProps> = (props) => {
+  return <ModalButtonTemplate {...props} />;
 };
 
 export const modal = (props) => {
