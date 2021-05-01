5f2fb8363f3429dece3961fadd725f3959830cae
Kenichi Okiebisu <a01056715@gmail.com>
2021-04-30 20:56:45 -0700

    feat: closed button

--- packages/web/components/atoms/button/button-closed/template.tsx
+++ packages/web/components/atoms/button/button-closed/template.tsx
@@ -1,39 +1,27 @@
 import { Icon } from '@icons';
+import { ActionIconTypes } from 'components/icons/action';
+import { SemanticIconTypes } from 'components/icons/semantic';
 
 export interface ClosedButtonTemplateProps {
   selected?: boolean;
-  closedType?: 'close' | 'check';
+  icon?: {
+    variant: 'action' | 'semantic';
+    actionType?: ActionIconTypes;
+    semanticType?: SemanticIconTypes;
+  };
 }
 
 export const ClosedButtonTemplate: React.FC<ClosedButtonTemplateProps> = ({
-  closedType,
+  icon,
   selected,
 }) => {
-  const types = {
-    close: (
-      <Icon
-        variant="action"
-        actionType="close"
-        width={16}
-        fill={selected ? 'white' : 'black'}
-      />
-    ),
-    check: (
-      <Icon
-        variant="semantic"
-        semanticType="check"
-        width={16}
-        fill={selected ? 'white' : 'black'}
-      />
-    ),
-  };
   return (
     <div
       className={`inline-flex items-center justify-center p-2 rounded-full border ${
         selected ? 'bg-black border-black' : 'bg-white border-gray-300'
       }`}
     >
-      {types[closedType]}
+      <Icon {...icon} width={16} fill={selected ? 'white' : 'black'} />
     </div>
   );
 };
