9fc83d598db87300c303abb3e7b8855d477f136b
Kenichi Okiebisu <a01056715@gmail.com>
2021-05-07 07:36:56 -0700

    refactor: header

--- packages/web/components/organisms/header/header-homes/template.tsx
+++ packages/web/components/organisms/header/header-homes/template.tsx
@@ -1,13 +1,17 @@
 import Router from 'next/router';
 import { Button } from '@button';
 
+export interface HomesHeaderTemplateProps {
+  spread?: boolean;
+}
+
 /**
  * Renders the header for the homes page
  * @param {boolean} spread - Whether if the layout should be spread out or not
  */
-export const HomesHeader: React.FC<{
-  spread?: boolean;
-}> = ({ spread = false }) => {
+export const HomesHeaderTemplate: React.FC<HomesHeaderTemplateProps> = ({
+  spread = false,
+}) => {
   return (
     <header className="px-0 h-48">
       <div style={{ padding: 20 }} className="h-full">
