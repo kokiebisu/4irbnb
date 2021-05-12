170d3785b6f2f795be6680a02acb0c2c462b6a51
Kenichi Okiebisu <a01056715@gmail.com>
2021-05-12 08:57:55 -0700

    fix: stays service lint

--- services/stays/src/routes/update.ts
+++ services/stays/src/routes/update.ts
@@ -1,23 +1,28 @@
-import express, { Request, Response } from "express";
-import { NotAuthorizedError, NotFoundError } from "@nextbnb/error";
-import { requireAuth } from "@nextbnb/middleware";
-import { Stay } from "../models/stay";
+import * as express from 'express';
+import { Request, Response } from 'express';
+import { NotAuthorizedError, NotFoundError } from '@nextbnb/error';
+import { requireAuth } from '@nextbnb/middleware';
+import { Stay } from '../models/stay';
 
 const router = express.Router();
 
-router.put(
-  "/api/stays/:id",
-  requireAuth,
-  async (req: Request, res: Response) => {
-    const stay = await Stay.findById(req.params.id);
-    if (!stay) {
-      throw new NotFoundError();
-    }
-    if (stay.userId !== req.currentUser!.id) {
-      throw new NotAuthorizedError();
-    }
-    res.status(200).send(req.body);
+async function updateStay(req: Request, res: Response): Promise<void> {
+  const stay = await Stay.findById(req.params.id);
+  if (!stay) {
+    throw new NotFoundError();
   }
-);
+  if (req.currentUser && stay.userId !== req.currentUser.id) {
+    throw new NotAuthorizedError();
+  }
+  res.status(200).send(req.body);
+}
+
+router.put('/api/stays/:id', requireAuth, (req: Request, res: Response) => {
+  updateStay(req, res).catch((err: unknown) => {
+    res.status(400).json({
+      message: err,
+    });
+  });
+});
 
 export { router as UpdateRouter };
