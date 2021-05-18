1de4b81031a99e5e4c3629a1787dc5175f83ded3
Kenichi Okiebisu <a01056715@gmail.com>
2021-05-17 19:47:46 -0700

    fix: button

--- app/web/components/atoms/button/button-calendar/index.tsx
+++ app/web/components/atoms/button/button-calendar/index.tsx
@@ -5,11 +5,3 @@ export interface CalendarButtonProps {}
 export const CalendarButton: React.FC<CalendarButtonProps> = ({ ...props }) => {
   return <CalendarButtonTemplate {...props} />;
 };
-
-export const calendar = (props) => {
-  return {
-    calendar: {
-      component: <CalendarButton {...props} />,
-    },
-  };
-};
