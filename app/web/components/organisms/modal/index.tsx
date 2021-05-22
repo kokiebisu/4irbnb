7aa6fc679305070c566605e687623be2e8c14746
Kenichi Okiebisu <a01056715@gmail.com>
2021-05-22 12:01:34 -0700

    fix

--- app/web/components/organisms/modal/index.tsx
+++ app/web/components/organisms/modal/index.tsx
@@ -2,39 +2,36 @@ import { useRef } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import useOnClickOutside from '@hooks/useOnClickOutside';
 import { useLockBodyScroll } from '@hooks/useLockBodyScroll';
-import { MenuModal } from '@modal/modal-menu';
-import { PrivacyModal } from '@modal/modal-privacy';
-import { AuthModal } from '@modal/modal-auth';
-import { BookingModal } from '@modal/modal-booking';
-import { GlobeModal } from '@modal/modal-globe';
-import { LocationModal } from '@modal/modal-location';
-import { GuestsModal } from '@modal/modal-guests';
-import { CheckModal } from '@modal/modal-check';
-import { ListingModal } from '@modal/modal-listing';
+import { MenuModalProps } from '@modal/modal-menu';
+import { PrivacyModalProps } from '@modal/modal-privacy';
+import { AuthModalProps } from '@modal/modal-auth';
+import { BookingModalProps } from '@modal/modal-booking';
+import { GlobeModalProps } from '@modal/modal-globe';
+import { LocationModalProps } from '@modal/modal-location';
+import { GuestsModalProps } from '@modal/modal-guests';
+import { CheckModalProps } from '@modal/modal-check';
+import { ListingModalProps } from '@modal/modal-listing';
 import { useToggleDispatch } from '@context/toggle';
+import { factory } from './utils/factory';
 
-export type ModalVariants =
-  | 'privacy'
-  | 'menu'
-  | 'auth'
-  | 'booking'
-  | 'globe'
-  | 'location'
-  | 'guests'
-  | 'checkin'
-  | 'checkout'
-  | 'listing';
-
-export interface ModalProps {
-  variant: ModalVariants;
-  extendsTo?: string;
-  dispatch?: any;
-  animate?: string;
+export type ModalProps = (
+  | ({ variant: 'privacy' } & PrivacyModalProps)
+  | ({ variant: 'menu' } & MenuModalProps)
+  | ({ variant: 'auth' } & AuthModalProps)
+  | ({ variant: 'booking' } & BookingModalProps)
+  | ({ variant: 'globe' } & GlobeModalProps)
+  | ({ variant: 'location' } & LocationModalProps)
+  | ({ variant: 'guests' } & GuestsModalProps)
+  | ({ variant: 'checkin' } & CheckModalProps)
+  | ({ variant: 'checkout' } & CheckModalProps)
+  | ({ variant: 'listing' } & ListingModalProps)
+) & {
+  criteria?: boolean;
   lock?: boolean;
-  criteria?: any;
   noPadding?: boolean;
-  [property: string]: any;
-}
+  animate: string;
+  dispatch: string;
+};
 
 /**
  * Bundles all the modal components
@@ -43,8 +40,6 @@ export interface ModalProps {
  * @param {string} dispatchType - The type of dispatch
  */
 export const Modal: React.FC<ModalProps> = ({
-  variant,
-  extendsTo,
   dispatch,
   animate = 'default',
   lock = false,
@@ -57,9 +52,9 @@ export const Modal: React.FC<ModalProps> = ({
   if (lock) {
     useLockBodyScroll();
   }
-  useOnClickOutside(containerRef, () =>
-    toggleDispatch({ type: `${dispatch}` })
-  );
+  useOnClickOutside(containerRef, () => {
+    toggleDispatch({ type: `${dispatch as string}` });
+  });
 
   const animation = {
     slideup: {
@@ -74,61 +69,12 @@ export const Modal: React.FC<ModalProps> = ({
     },
   };
 
-  const variants: {
-    [variant: string]: {
-      component: JSX.Element;
-      extendsTo?: string;
-      noPadding?: boolean;
-    };
-  } = {
-    privacy: {
-      component: <PrivacyModal {...props} />,
-      extendsTo: 'rounded-md p-5 lg:w-full max-w-[1600px]',
-    },
-    menu: {
-      component: <MenuModal {...props} />,
-      extendsTo: 'rounded-lg w-96',
-    },
-    auth: {
-      component: <AuthModal {...props} />,
-      extendsTo: `rounded-lg w-full sm:w-[550px]`,
-    },
-    booking: {
-      component: <BookingModal {...props} />,
-      extendsTo: 'p-4 border border-gray-600 rounded-lg',
-    },
-    globe: {
-      component: <GlobeModal {...props} />,
-      extendsTo: 'p-4 rounded-lg max-w-3xl h-fit',
-    },
-    location: {
-      component: <LocationModal {...props} />,
-      extendsTo: 'rounded-lg p-5 max-w-sm',
-    },
-    guests: {
-      component: <GuestsModal {...props} />,
-      extendsTo: 'p-5 rounded-lg max-w-xs',
-    },
-    checkin: {
-      component: <CheckModal {...props} />,
-      extendsTo: 'px-7 py-5  rounded-lg max-w-2.5xl',
-    },
-    checkout: {
-      component: <CheckModal {...props} />,
-      extendsTo: 'rounded-lg px-7 py-5 max-w-2.5xl',
-    },
-    listing: {
-      component: <ListingModal {...props} />,
-      extendsTo: 'p-5 rounded-lg max-w-lg',
-    },
-  };
-
   if (criteria !== undefined) {
     return (
       <AnimatePresence>
         {criteria && (
           <motion.div
-            data-testid={`${variant}-modal`}
+            data-testid={`${props.variant as string}-modal`}
             exit={{ opacity: 0 }}
             initial={animation[animate].initial}
             animate={animation[animate].animate}
@@ -136,7 +82,7 @@ export const Modal: React.FC<ModalProps> = ({
             ref={containerRef}
             className={`shadow-sm bg-white w-full ${variants[variant].extendsTo} ${extendsTo}`}
           >
-            {variants[variant].component}
+            {factory(props)}
           </motion.div>
         )}
       </AnimatePresence>
@@ -148,7 +94,7 @@ export const Modal: React.FC<ModalProps> = ({
       className={`shadow-sm bg-white w-full ${variants[variant].extendsTo} ${extendsTo}`}
       data-testid={`${variant}-modal`}
     >
-      {variants[variant].component}
+      {factory(props)}
     </div>
   );
 };
