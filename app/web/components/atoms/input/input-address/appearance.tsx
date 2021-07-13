71e65c3ba8ad4fde336158ec93acdbb8609b897c
kokiebisu <a01056715@gmail.com>
2021-07-12 20:02:09 -0700

    feat: input{address, another, birthdate

--- app/web/components/atoms/input/input-address/appearance.tsx
+++ app/web/components/atoms/input/input-address/appearance.tsx
@@ -1,12 +1,16 @@
-export const useAddressInputAppearance = ({ direction }) => {
+export const useAddressInputAppearance = ({
+  direction,
+}: {
+  direction: "top" | "bottom";
+}) => {
   const renderShape = (direction: string) => {
     switch (direction) {
-      case 'top':
-        return 'border-b border-l border-r border-gray-400 rounded-br-lg';
-      case 'bottom':
-        return 'border-t border-l border-r rounded-t-lg';
+      case "top":
+        return "border-b border-l border-r border-gray-400 rounded-br-lg";
+      case "bottom":
+        return "border-t border-l border-r rounded-t-lg";
       default:
-        return 'border border-gray-400 rounded-lg';
+        return "border border-gray-400 rounded-lg";
     }
   };
 
