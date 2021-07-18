3f6544b2783d32ca9e01a2615bbceda39b59cde9
kokiebisu <a01056715@gmail.com>
2021-07-18 11:12:24 -0700

    fix: script

--- terraform/resources/subnets.tf
+++ terraform/resources/subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_subnet" {
     availability_zone = "us-east-1a"
     cidr_block = "10.0.1.0/24"
-    vpc_id = aws_vpc.nextbnb-vpc.id
+    vpc_id = aws_vpc.nextbnb_vpc.id
 
     tags = {
         Name = "${var.app_name}-public-subnet"
