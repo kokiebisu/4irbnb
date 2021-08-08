2bec76fa8abd86b82e77af93d3675dd486ea8d57
kokiebisu <a01056715@gmail.com>
2021-08-08 16:42:24 -0700

    fix

--- packages/stay/terraform/monitor.tf
+++ packages/stay/terraform/monitor.tf
@@ -1,11 +1,11 @@
 resource "aws_cloudwatch_log_group" "stay_api" {
-  name = "/aws/lambda/4irbnb/${aws_lambda_function.stay_api.function_name}"
+  name = "/aws/lambda/${aws_lambda_function.stay_api.function_name}"
 
   retention_in_days = 30
 }
 
 resource "aws_cloudwatch_log_group" "stay_consumer" {
-  name = "/aws/lambda/4irbnb/${aws_lambda_function.stay_consumer.function_name}"
+  name = "/aws/lambda/${aws_lambda_function.stay_consumer.function_name}"
 
   retention_in_days = 30
 }
