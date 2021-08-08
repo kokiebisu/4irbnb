03b93e62ff30e9572bfa4f71db3d76b0f49810a1
kokiebisu <a01056715@gmail.com>
2021-08-08 15:27:21 -0700

    fix

--- packages/user/terraform/lambda.tf
+++ packages/user/terraform/lambda.tf
@@ -1,21 +1,3 @@
-data "aws_ecr_repository" "user_api" {
-  name = "4irbnb-user-api"
-}
-
-data "aws_ecr_repository" "user_consumer" {
-  name = "4irbnb-user-consumer"
-}
-
-data "aws_ecr_image" "user_api" {
-  repository_name = data.aws_ecr_repository.user_api.name
-  image_tag = "latest"
-}
-
-data "aws_ecr_image" "user_consumer" {
-  repository_name = data.aws_ecr_repository.user_consumer.name
-  image_tag = "latest"
-}
-
 resource "aws_lambda_function" "user_api" {
   function_name = "4irbnb-user-api"
   role          = var.service_role_arn
