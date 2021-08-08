36a1baaec4180a5a8ca291d739ed8185fb1e80fe
kokiebisu <a01056715@gmail.com>
2021-08-08 15:56:55 -0700

    fix: terraform

--- packages/user/terraform/monitor.tf
+++ packages/user/terraform/monitor.tf
@@ -1,10 +1,10 @@
-resource "aws_cloudwatch_log_group" "stay" {
+resource "aws_cloudwatch_log_group" "user_api" {
   name = "/aws/lambda/4irbnb/${aws_lambda_function.user_api.function_name}"
 
   retention_in_days = 30
 }
 
-resource "aws_cloudwatch_log_group" "stay" {
+resource "aws_cloudwatch_log_group" "user_consumer" {
   name = "/aws/lambda/4irbnb/${aws_lambda_function.user_consumer.function_name}"
 
   retention_in_days = 30
