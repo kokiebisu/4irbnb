fa1cd573b4b14591f28a21fc1e451072e8cb9363
kokiebisu <a01056715@gmail.com>
2021-08-08 15:50:57 -0700

    fix: terraform lambda permission

--- packages/stay/terraform/lambda.tf
+++ packages/stay/terraform/lambda.tf
@@ -12,10 +12,18 @@ resource "aws_lambda_function" "stay_consumer" {
   image_uri = "${data.aws_ecr_repository.stay_consumer.repository_url}@${data.aws_ecr_image.stay_consumer.id}"
 }
 
-resource "aws_lambda_permission" "stay" {
+resource "aws_lambda_permission" "stay_api" {
     statement_id = "AllowExecutionFromAPIGateway"
     action = "lambda:InvokeFunction"
     function_name = aws_lambda_function.stay_api.function_name
+    principal = "apigateway.amazonaws.com"
+
+    source_arn = "${var.api_gateway_execution_arn}/*/*"
+}
+resource "aws_lambda_permission" "stay_consumer" {
+    statement_id = "AllowExecutionFromAPIGateway"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.stay_consumer.function_name
     principal = "apigateway.amazonaws.com"
 
     source_arn = "${var.api_gateway_execution_arn}/*/*"
