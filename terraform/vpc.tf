59b1b3d15ef91ff081e4acf4da8cdc1546bf1081
kokiebisu <a01056715@gmail.com>
2021-08-19 08:37:47 -0700

    fix

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -12,21 +12,25 @@ resource "aws_security_group" "rds" {
   name        = "rds"
   description = "Allows RDS to be accessed from anywhere"
   vpc_id      = aws_vpc.vpc.id
+}
 
-  ingress = {
-      description = "Allows mysql protocols to enter to the VPC"
-      from_port   = 3306
-      to_port     = 3306
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = [aws_vpc.vpc.ipv6_cidr_block]
-    }
+resource "aws_security_group_rule" "example" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.rds
+}
 
-  egress = {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
+
+resource "aws_security_group_rule" "example" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.rds
 }
