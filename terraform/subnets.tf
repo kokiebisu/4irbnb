3158f1f646ff9181c9af9d598ef90ea5fa1352d3
kokiebisu <a01056715@gmail.com>
2021-08-20 07:58:27 -0700

    fix

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,28 +1,38 @@
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_a" {
   availability_zone = "us-east-1a"
   cidr_block        = "10.0.1.0/24"
   vpc_id            = aws_vpc.vpc.id
 
   tags = {
-    Name = "${var.app_name}-public-subnet"
+    Name = "${var.app_name}-public-a-subnet"
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "public_b" {
+  availability_zone = "us-east-1b"
+  cidr_block        = "10.0.1.0/24"
+  vpc_id            = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.app_name}-public-b-subnet"
+  }
+}
+
+resource "aws_subnet" "private_b" {
   availability_zone = "us-east-1b"
   cidr_block        = "10.0.2.0/24"
   vpc_id            = aws_vpc.vpc.id
 
   tags = {
-    Name = "${var.app_name}-private-subnet"
+    Name = "${var.app_name}-private-b-subnet"
   }
 }
 
 resource "aws_db_subnet_group" "rds" {
   name       = "rds_subnet_group"
-  subnet_ids = [aws_subnet.public.id]
+  subnet_ids = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 
   tags = {
-    Name = "Education"
+    Name = "4irbnb Relational DB"
   }
 }
