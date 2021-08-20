3236cb76884e1bbf2738e860cf11a0c210325c2e
kokiebisu <a01056715@gmail.com>
2021-08-20 08:09:24 -0700

    fix

--- terraform/route-tables.tf
+++ terraform/route-tables.tf
@@ -17,7 +17,7 @@ resource "aws_route_table_association" "public_route_a_table" {
   subnet_id      = aws_subnet.public_a.id
 }
 
-resource "aws_route_table_association" "public_route_a_table" {
+resource "aws_route_table_association" "public_route_b_table" {
   route_table_id = aws_default_route_table.public_route_table.id
   subnet_id      = aws_subnet.public_b.id
 }
