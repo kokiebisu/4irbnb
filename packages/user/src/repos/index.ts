4d460cc49465213ea55df7e9ce9d795bb9667ed5
kokiebisu <a01056715@gmail.com>
2021-09-19 17:14:23 +0900

    refactor(user): moved mock repo inside test folder

--- packages/user/src/repos/index.ts
+++ packages/user/src/repos/index.ts
@@ -1,2 +1,1 @@
-export * as Mock from "./mock";
 export * as RepositoryTypes from "./types";
--- packages/user/src/repos/types.ts
+++ packages/user/src/repos/types.ts
@@ -2,5 +2,5 @@ import { IBaseRepository } from "@4irbnb/common";
 import { Entity } from "../domains";
 
 export interface IRepository extends IBaseRepository<Entity> {
-  findByEmail(email: string): Promise<Entity>;
+  findByEmail(email: string): Promise<Entity | null>;
 }
