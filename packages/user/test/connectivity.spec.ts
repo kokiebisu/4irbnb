b1c3b56f7af8648b5e16e3c7d28e880be7087f75
kokiebisu <a01056715@gmail.com>
2021-09-26 15:10:47 +0900

    fix: test

--- packages/user/test/connectivity.spec.ts
+++ packages/user/test/connectivity.spec.ts
@@ -1,25 +1,15 @@
 import { Identifier } from "@4irbnb/common";
 import { Email } from "../src/domains/fields";
 import { Repository } from "../src/repos";
-import { IRepository } from "../src/repos/types";
-
-let repo: IRepository;
-
-beforeAll(async () => {
-  repo = await Repository.initialize();
-  await repo.openConnection();
-});
-
-afterAll(async () => {
-  await repo.closeConnection();
-});
 
 describe("Connectivity", () => {
   it("Finds by ID", async () => {
+    const repo = await Repository.initialize();
     const user = await repo.findById(new Identifier("1"));
     expect(user).not.toBeNull();
   });
   it("Finds by Email", async () => {
+    const repo = await Repository.initialize();
     const user = await repo.findByEmail(Email.create("kenichikona@gmail.com"));
     expect(user).not.toBeNull();
   });
