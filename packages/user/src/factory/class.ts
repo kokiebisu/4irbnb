e10656e3622c34335d6baabe1bad36e3c1e70c38
kokiebisu <a01056715@gmail.com>
2021-09-26 15:47:50 +0900

    feat(user): added logic to factory so it fetches the next identifer from db instead of using uuid

--- packages/user/src/factory/class.ts
+++ packages/user/src/factory/class.ts
@@ -1,14 +1,26 @@
-import { v4 as uuid } from "uuid";
 import { Identifier } from "@4irbnb/common";
 import { Entity } from "../domains";
 import { IEntityProps } from "../domains/types";
+import { IRepository } from "../repos/types";
 
 /**
  * @public Creates the User domain object. During the process, it attaches the appropriate identifier
  */
 export class Factory {
-  public create(props: IEntityProps) {
-    const id = new Identifier(uuid());
-    return new Entity(id, props);
+  #repo: IRepository;
+
+  public constructor(repo: IRepository) {
+    this.#repo = repo;
+  }
+
+  /**
+   * Finds the next primary identifer and attaches it to the Entity to be created so that the primary identifier aligns with the one with the database
+   * @param props
+   * @returns
+   */
+  public async create(props: IEntityProps) {
+    const id = await this.#repo.findNextIdentifier();
+    const nextIdentifier = new Identifier(id);
+    return new Entity(nextIdentifier, props);
   }
 }
